c90c288a02b15fabd8fbcaee56a083bb2f6dd76e
Peter Abeles <peter.abeles@gmail.com>
2011-08-09 20:31:59 -0400

    - Added unit test for FastHessianFeatureDetector

--- feature/test/gecv/alg/detect/interest/GenericFeatureScaleDetector.java
+++ feature/test/gecv/alg/detect/interest/GenericFeatureScaleDetector.java
@@ -22,97 +22,29 @@ import gecv.abst.detect.corner.WrapperLaplacianBlobIntensity;
 import gecv.abst.detect.extract.FactoryFeatureFromIntensity;
 import gecv.abst.detect.extract.FeatureExtractor;
 import gecv.alg.detect.corner.HessianBlobIntensity;
-import gecv.core.image.GeneralizedImageOps;
 import gecv.struct.image.ImageFloat32;
-import jgrl.geometry.UtilPoint2D_I32;
-import org.junit.Test;
-
-import java.util.List;
-import java.util.Random;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
 /**
  * @author Peter Abeles
  */
-public abstract class GenericFeatureScaleDetector {
-
-		Random rand = new Random(234);
-	int width = 20;
-	int height = 30;
+public abstract class GenericFeatureScaleDetector extends GenericFeatureDetector {
 
 	int r = 2;
 
-	/**
-	 * Checks to see if features are flushed after multiple calls
-	 */
-	@Test
-	public void checkFlushFeatures() {
-		double scales[]=new double[]{1,2,4,8};
-		ImageFloat32 input = new ImageFloat32(width,height);
-		GeneralizedImageOps.fillRectangle(input,20,10,10,width,height);
-
+	private GeneralFeatureDetector<ImageFloat32,ImageFloat32> createBlobDetector() {
 		FeatureExtractor extractor = FactoryFeatureFromIntensity.create(r,1,0,false,false,false);
 		GeneralFeatureIntensity<ImageFloat32, ImageFloat32> intensity =
 				new WrapperLaplacianBlobIntensity<ImageFloat32,ImageFloat32>(HessianBlobIntensity.Type.DETERMINANT,ImageFloat32.class);
-		GeneralFeatureDetector<ImageFloat32,ImageFloat32> detector =
-				new GeneralFeatureDetector<ImageFloat32,ImageFloat32>(intensity,extractor,200);
-
-		// give it one corner to find
-		GeneralizedImageOps.fill(input,50);
-		drawCircle(input,10,10,r*2);
-
-		Object alg = createDetector(detector);
-		int firstFound = detectFeature(input, scales,alg).size();
-		int secondFound = detectFeature(input, scales,alg).size();
-
-		// if features are not flushed then the secondFound should be twice as large
-		assertEquals(firstFound,secondFound);
+		return new GeneralFeatureDetector<ImageFloat32,ImageFloat32>(intensity,extractor,200);
 	}
 
-	/**
-	 * Very basic test that just checks to see if it can find an obvious circular feature
-	 */
-	@Test
-	public void basicDetect() {
-		double scales[]=new double[]{1,2,4,8};
-		ImageFloat32 input = new ImageFloat32(width,height);
-		GeneralizedImageOps.fillRectangle(input,20,10,10,width,height);
-
-		FeatureExtractor extractor = FactoryFeatureFromIntensity.create(r,1,0,false,false,false);
-		GeneralFeatureIntensity<ImageFloat32, ImageFloat32> intensity =
-				new WrapperLaplacianBlobIntensity<ImageFloat32,ImageFloat32>(HessianBlobIntensity.Type.DETERMINANT,ImageFloat32.class);
-		GeneralFeatureDetector<ImageFloat32,ImageFloat32> detector =
-				new GeneralFeatureDetector<ImageFloat32,ImageFloat32>(intensity,extractor,200);
-
-		// give it one corner to find
-		GeneralizedImageOps.fill(input,50);
-		drawCircle(input,10,10,r*2);
-
-		Object alg = createDetector(detector);
-		List<ScalePoint> found = detectFeature(input,scales,alg);
-
-		assertTrue(found.size()==1);
-		ScalePoint p = found.get(0);
-		assertEquals(2,p.scale,1e-4);
-		assertEquals(10,p.x,r);
-		assertEquals(10,p.y,r);
+	@Override
+	protected Object createDetector()
+	{
+		return createDetector(createBlobDetector());
 	}
 
 	protected abstract Object createDetector( GeneralFeatureDetector<ImageFloat32, ImageFloat32> detector);
 
-	protected abstract List<ScalePoint> detectFeature(ImageFloat32 input, double[] scales, Object detector);
 
-	private void drawCircle( ImageFloat32 img , int c_x , int c_y , double r ) {
-
-		for( int y = 0; y < img.height; y++ ) {
-			for( int x = 0; x < img.width; x++ ) {
-				double d = UtilPoint2D_I32.distance(x,y,c_x,c_y);
-				if( d <= r ) {
-					img.set(x,y,0);
-				}
-			}
-		}
-	}
 }
