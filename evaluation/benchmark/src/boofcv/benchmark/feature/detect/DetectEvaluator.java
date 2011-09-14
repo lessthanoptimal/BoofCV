912531ea4f9e3cea2bed8a514c09c625e6e88ea7
Peter Abeles <peter.abeles@gmail.com>
2011-09-13 22:52:41 -0400

    - Reorganized evaluation - moved xuggler test over

---
+++ evaluation/benchmark/src/boofcv/benchmark/feature/detect/DetectEvaluator.java
@@ -0,0 +1,125 @@
+/*
+ * Copyright (c) 2011, Peter Abeles. All Rights Reserved.
+ *
+ * This file is part of BoofCV (http://www.boofcv.org).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package boofcv.benchmark.feature.detect;
+
+import boofcv.abst.feature.detect.interest.InterestPointDetector;
+import boofcv.benchmark.feature.BenchmarkAlgorithm;
+import boofcv.benchmark.feature.distort.DistortParam;
+import boofcv.benchmark.feature.distort.StabilityEvaluator;
+import boofcv.benchmark.feature.distort.StabilityEvaluatorPoint;
+import boofcv.struct.image.ImageBase;
+import georegression.geometry.UtilPoint2D_I32;
+import georegression.struct.affine.Affine2D_F32;
+import georegression.struct.point.Point2D_F32;
+import georegression.struct.point.Point2D_I32;
+import georegression.transform.affine.AffinePointOps;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * @author Peter Abeles
+ */
+public class DetectEvaluator<T extends ImageBase> implements StabilityEvaluator<T> {
+
+	// two points are considered to be the same if they are within this tolerance
+	double matchTolerance = 3;
+
+	// location where features were originally detected
+	List<Point2D_I32> original;
+
+	@Override
+	public String[] getMetricNames() {
+		return new String[]{"Found","Percent"};
+	}
+
+	@Override
+	public void extractInitial(BenchmarkAlgorithm alg, T image)
+	{
+		InterestPointDetector<T> detector = alg.getAlgorithm();
+		detector.detect(image);
+		original = createDetectionList(detector);
+	}
+
+	@Override
+	public double[] evaluateImage(BenchmarkAlgorithm alg, T image, DistortParam param )
+	{
+		Affine2D_F32 initToImage = StabilityEvaluatorPoint.createTransform(param.scale,param.rotation,image);
+
+		// move the found points to the new coordinate system or just use the
+		List<Point2D_I32> original = transformOriginal(image,initToImage);
+
+		InterestPointDetector<T> detector = alg.getAlgorithm();
+		detector.detect(image);
+		List<Point2D_I32> found = createDetectionList(detector);
+
+		int numMissed = 0;
+
+		for( Point2D_I32 origPt : original ) {
+			double bestDist = Double.MAX_VALUE;
+			for( Point2D_I32 p : found ) {
+				double d = UtilPoint2D_I32.distance(origPt,p);
+
+				if( d < bestDist ) {
+					bestDist = d;
+				}
+			}
+
+			if( bestDist > matchTolerance ) {
+				numMissed++;
+			}
+		}
+
+		double[] results = new double[3];
+
+		// number found
+		results[0] = original.size();
+		// fraction matched
+		results[1] = 100.0*(original.size()-numMissed)/(double)original.size();
+
+		return results;
+	}
+
+	private List<Point2D_I32> transformOriginal( T image , Affine2D_F32 initToImage )
+	{
+		List<Point2D_I32> ret = new ArrayList<Point2D_I32>();
+
+		Point2D_F32 f = new Point2D_F32();
+		Point2D_F32 t = new Point2D_F32();
+
+		for( Point2D_I32 p : original ) {
+			f.x = p.x;
+			f.y = p.y;
+			AffinePointOps.transform(initToImage,f,t);
+			if( image.isInBounds((int)t.x,(int)t.y) ) {
+				ret.add( new Point2D_I32((int)t.x,(int)t.y));
+			}
+		}
+		return ret;
+	}
+
+	public List<Point2D_I32> createDetectionList( InterestPointDetector det ) {
+		List<Point2D_I32> list = new ArrayList<Point2D_I32>();
+		for( int i = 0; i < det.getNumberOfFeatures(); i++ ) {
+			list.add(det.getLocation(i).copy());
+		}
+		return list;
+	}
+}
---
+++ evaluation/visualization/src/boofcv/alg/interpolate/EvaluateInterpolateEnlargeApp.java
@@ -0,0 +1,91 @@
+/*
+ * Copyright (c) 2011, Peter Abeles. All Rights Reserved.
+ *
+ * This file is part of BoofCV (http://www.boofcv.org).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package boofcv.alg.interpolate;
+
+import boofcv.alg.distort.DistortImageOps;
+import boofcv.core.image.ConvertBufferedImage;
+import boofcv.core.image.GeneralizedImageOps;
+import boofcv.factory.interpolate.FactoryInterpolation;
+import boofcv.gui.ListDisplayPanel;
+import boofcv.gui.image.ShowImages;
+import boofcv.io.image.UtilImageIO;
+import boofcv.struct.image.ImageBase;
+import boofcv.struct.image.ImageFloat32;
+import boofcv.struct.image.ImageUInt8;
+
+import java.awt.image.BufferedImage;
+
+/**
+ * Compares different types of interpolation algorithms by enlarging an image.
+ *
+ * @author Peter Abeles
+ */
+public class EvaluateInterpolateEnlargeApp<T extends ImageBase> {
+
+	T grayOriginal;
+	int scaledWidth;
+	int scaledHeight;
+
+	ListDisplayPanel scalePanel = new ListDisplayPanel();
+
+	public EvaluateInterpolateEnlargeApp( BufferedImage input ,
+										  int scaledWidth , int scaledHeight ,
+										  Class<T> imageType ) {
+		grayOriginal = ConvertBufferedImage.convertFrom(input,null,imageType);
+		this.scaledWidth = scaledWidth;
+		this.scaledHeight = scaledHeight;
+		scalePanel.addImage(input,"Original");
+		scalePanel.setDataDisplaySize(scaledWidth,scaledHeight);
+		ShowImages.showWindow(scalePanel,"Scaled Images");
+
+	}
+
+	public void addInterpolation( String name , InterpolatePixel<T> alg ) {
+		T scaledImg = (T)grayOriginal._createNew(scaledWidth,scaledHeight);
+
+		DistortImageOps.scale(grayOriginal,scaledImg,alg);
+
+		// numerical round off error can cause the interpolation to go outside
+		// of pixel value bounds
+		GeneralizedImageOps.boundImage(scaledImg,0,255);
+
+		BufferedImage out = ConvertBufferedImage.convertTo(scaledImg,null);
+		scalePanel.addImage(out,name);
+	}
+
+	public static void main( String args[] ) {
+
+		BufferedImage image = UtilImageIO.loadImage("data/eye01.jpg");
+
+		int w = image.getWidth()*8;
+		int h = image.getHeight()*8;
+
+		EvaluateInterpolateEnlargeApp<ImageFloat32> app = new EvaluateInterpolateEnlargeApp<ImageFloat32>(image,w,h, ImageFloat32.class);
+
+		app.addInterpolation("NN F32",FactoryInterpolation.nearestNeighborPixel(ImageFloat32.class));
+		app.addInterpolation("Bilinear F32",FactoryInterpolation.bilinearPixel(ImageFloat32.class));
+		app.addInterpolation("Bicubic F32", FactoryInterpolation.bicubic(ImageFloat32.class,-0.5f));
+
+		EvaluateInterpolateEnlargeApp<ImageUInt8> appU8 = new EvaluateInterpolateEnlargeApp<ImageUInt8>(image,w,h, ImageUInt8.class);
+
+		appU8.addInterpolation("NN U8",FactoryInterpolation.nearestNeighborPixel(ImageUInt8.class));
+		appU8.addInterpolation("Bilinear U8",FactoryInterpolation.bilinearPixel(ImageUInt8.class));
+		appU8.addInterpolation("Bicubic U8",FactoryInterpolation.bicubic(ImageUInt8.class,-0.5f));
+	}
+}
