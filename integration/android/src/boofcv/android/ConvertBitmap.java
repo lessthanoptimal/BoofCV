82fab57612c8a25f08d55a98a5045bb83155933f
Peter Abeles <peter.abeles@gmail.com>
2012-07-21 20:50:27 -0400

    Added android integration code and build

---
+++ integration/android/src/boofcv/android/ConvertBitmap.java
@@ -0,0 +1,244 @@
+package boofcv.android;
+
+import java.nio.ByteBuffer;
+
+import android.graphics.Bitmap;
+import boofcv.struct.image.ImageFloat32;
+import boofcv.struct.image.ImageSingleBand;
+import boofcv.struct.image.ImageUInt8;
+import boofcv.struct.image.MultiSpectral;
+
+/**
+ * Functions for converting Android Bitmap images into BoofCV formats.  In earlier versions of Android
+ * there is no way to directly access the internal array used by Bitmap.  You have to provide an array for it to
+ * be copied into.  This is why the storage array is provided.
+ * 
+ * @author Peter Abeles
+ *
+ */
+public class ConvertBitmap {
+	
+	/**
+	 * Declares a storage array for the provided Bitmap image.  The array will be
+	 * of size = width*height*byteCount where byteCount is the number of bytes per
+	 * pixel.
+	 * 
+	 * @param input Bitmap image.
+	 * @param storage If not null then the array size will be checked, if too small a new array will be returned.
+	 * @return An array of appropriate size.
+	 */
+	public static byte[] declareStorage( Bitmap input , byte[] storage ) {
+		int byteCount = input.getConfig() == Bitmap.Config.ARGB_8888 ? 4 : 2;
+		int length = input.getWidth()*input.getHeight()*byteCount;
+		
+		if( storage == null || storage.length < length )
+			return new byte[ length ];
+		return storage;
+	}
+	
+	/**
+	 * Converts Bitmap image into a single band image of arbitrary type.
+	 * 
+	 * @param input Input Bitmap image.
+	 * @param output Output single band image.  If null a new one will be declared.
+	 * @param imageType Type of single band image.
+	 * @param storage Byte array used for internal storage. If null it will be declared internally. 
+	 * @return The converted gray scale image.
+	 */
+	public static <T extends ImageSingleBand> 
+	T bitmapToGray( Bitmap input , T output , Class<T> imageType , byte[] storage) {
+		if( imageType == ImageFloat32.class )
+			return (T)bitmapToGray(input,(ImageFloat32)output,storage);
+		else if( imageType == ImageUInt8.class )
+			return (T)bitmapToGray(input,(ImageUInt8)output,storage);
+		else
+			throw new IllegalArgumentException("Unsupported BoofCV Image Type");
+		
+	}
+	
+	/**
+	 * Converts Bitmap image into ImageUInt8.
+	 * 
+	 * @param input Input Bitmap image.
+	 * @param output Output image.  If null a new one will be declared.
+	 * @param storage Byte array used for internal storage. If null it will be declared internally. 
+	 * @return The converted gray scale image.
+	 */
+	public static ImageUInt8 bitmapToGray( Bitmap input , ImageUInt8 output , byte[] storage ) {
+		if( output == null ) {
+			output = new ImageUInt8( input.getWidth() , input.getHeight() );
+		} else if( output.getWidth() != input.getWidth() || output.getHeight() != input.getHeight() ) {
+			throw new IllegalArgumentException("Image shapes are not the same");
+		}
+		
+		if( storage == null )
+			storage = declareStorage(input,null);
+		input.copyPixelsToBuffer(ByteBuffer.wrap(storage));
+		
+		ImplConvertBitmap.arrayToGray(storage, input.getConfig(), output);
+		
+		return output;
+	}
+	
+	/**
+	 * Converts Bitmap image into ImageFloat32.
+	 * 
+	 * @param input Input Bitmap image.
+	 * @param output Output image.  If null a new one will be declared.
+	 * @param storage Byte array used for internal storage. If null it will be declared internally.
+	 * @return The converted gray scale image.
+	 */
+	public static ImageFloat32 bitmapToGray( Bitmap input , ImageFloat32 output , byte[] storage) {
+		if( output == null ) {
+			output = new ImageFloat32( input.getWidth() , input.getHeight() );
+		} else if( output.getWidth() != input.getWidth() || output.getHeight() != input.getHeight() ) {
+			throw new IllegalArgumentException("Image shapes are not the same");
+		}
+		
+		if( storage == null )
+			storage = declareStorage(input,null);
+		input.copyPixelsToBuffer(ByteBuffer.wrap(storage));
+		
+		ImplConvertBitmap.arrayToGray(storage, input.getConfig(), output);
+		
+		return output;
+	}
+	
+	/**
+	 * Converts Bitmap image into MultiSpectral image of the appropriate type.
+	 * 
+	 * @param input Input Bitmap image.
+	 * @param output Output image.  If null a new one will be declared.
+	 * @param type The type of internal single band image used in the MultiSpectral image.
+	 * @param storage Byte array used for internal storage. If null it will be declared internally.
+	 * @return The converted MultiSpectral image.
+	 */
+	public static <T extends ImageSingleBand>
+	MultiSpectral<T> bitmapToMS( Bitmap input , MultiSpectral<T> output , Class<T> type , byte[] storage ) {
+		if( output == null ) {
+			output = new MultiSpectral<T>( type , input.getWidth() , input.getHeight() , 4 );
+		} else if( output.getWidth() != input.getWidth() || output.getHeight() != input.getHeight() ) {
+			throw new IllegalArgumentException("Image shapes are not the same");
+		}
+
+		if( storage == null )
+			storage = declareStorage(input,null);
+		input.copyPixelsToBuffer(ByteBuffer.wrap(storage));
+		
+		if( type == ImageUInt8.class )
+			ImplConvertBitmap.arrayToMulti_U8(storage, input.getConfig(), (MultiSpectral)output);
+		else if( type == ImageFloat32.class )
+			ImplConvertBitmap.arrayToMulti_F32(storage, input.getConfig(), (MultiSpectral)output);
+		else
+			throw new IllegalArgumentException("Unsupported BoofCV Type");
+
+		return output;
+	}
+	
+	/**
+	 * Converts ImageSingleBand into Bitmap.
+	 * 
+	 * @param input Input gray scale image.
+	 * @param output Output Bitmap image.
+	 * @param storage Byte array used for internal storage. If null it will be declared internally.
+	 */
+	public static void grayToBitmap( ImageSingleBand input , Bitmap output , byte[] storage) {
+		if( input instanceof ImageUInt8 )
+			grayToBitmap((ImageUInt8)input,output,storage);
+		else if( input instanceof ImageFloat32 )
+			grayToBitmap((ImageFloat32)input,output,storage);
+		else
+			throw new IllegalArgumentException("Unsupported BoofCV Type");
+	}
+	
+	/**
+	 * Converts ImageSingleBand into Bitmap.
+	 * 
+	 * @param input Input gray scale image.
+	 * @param output Output Bitmap image.
+	 * @param storage Byte array used for internal storage. If null it will be declared internally.
+	 */
+	public static void grayToBitmap( ImageUInt8 input , Bitmap output , byte[] storage) {
+		if( output.getWidth() != input.getWidth() || output.getHeight() != input.getHeight() ) {
+			throw new IllegalArgumentException("Image shapes are not the same");
+		}
+		
+		if( storage == null )
+			storage = declareStorage(output,null);
+		
+		ImplConvertBitmap.grayToArray(input, storage,output.getConfig());
+		output.copyPixelsFromBuffer(ByteBuffer.wrap(storage));
+	}
+	
+	/**
+	 * Converts ImageSingleBand into Bitmap.
+	 * 
+	 * @param input Input gray scale image.
+	 * @param output Output Bitmap image.
+	 * @param storage Byte array used for internal storage. If null it will be declared internally.
+	 */
+	public static void grayToBitmap( ImageFloat32 input , Bitmap output , byte[] storage ) {
+		if( output.getWidth() != input.getWidth() || output.getHeight() != input.getHeight() ) {
+			throw new IllegalArgumentException("Image shapes are not the same");
+		}
+		
+		if( storage == null )
+			storage = declareStorage(output,null);
+		
+		ImplConvertBitmap.grayToArray(input, storage,output.getConfig());
+		output.copyPixelsFromBuffer(ByteBuffer.wrap(storage));
+	}
+	
+	/**
+	 * Converts MultiSpectral image into Bitmap.
+	 * 
+	 * @param input Input MultiSpectral image.
+	 * @param output Output Bitmap image.
+	 * @param storage Byte array used for internal storage. If null it will be declared internally.
+	 */
+	public static <T extends ImageSingleBand>
+	void multiToBitmap(  MultiSpectral<T> input , Bitmap output , byte[] storage ) {
+		if( output.getWidth() != input.getWidth() || output.getHeight() != input.getHeight() ) {
+			throw new IllegalArgumentException("Image shapes are not the same");
+		}
+		
+		if( storage == null )
+			storage = declareStorage(output,null);
+		
+		if( input.getType() == ImageUInt8.class )
+			ImplConvertBitmap.multiToArray_U8((MultiSpectral)input, storage,output.getConfig());
+		else if( input.getType() == ImageFloat32.class )
+			ImplConvertBitmap.multiToArray_F32((MultiSpectral)input, storage,output.getConfig());
+		else
+			throw new IllegalArgumentException("Unsupported BoofCV Type");
+		output.copyPixelsFromBuffer(ByteBuffer.wrap(storage));
+	}
+	
+	/**
+	 * Converts ImageUInt8 into a new Bitmap.
+	 * 
+	 * @param input Input gray scale image.
+	 * @param config Type of Bitmap image to create.
+	 */
+	public static Bitmap grayToBitmap( ImageUInt8 input , Bitmap.Config config ) {
+		Bitmap output = Bitmap.createBitmap(input.width, input.height, config);
+		
+		grayToBitmap(input,output,null);
+		
+		return output;
+	}
+	
+	/**
+	 * Converts ImageFloat32 into a new Bitmap.
+	 * 
+	 * @param input Input gray scale image.
+	 * @param config Type of Bitmap image to create.
+	 */
+	public static Bitmap grayToBitmap( ImageFloat32 input , Bitmap.Config config ) {
+		Bitmap output = Bitmap.createBitmap(input.width, input.height, config);
+		
+		grayToBitmap(input,output,null);
+		
+		return output;
+	}
+}
---
+++ integration/android/test/boofcv/android/TestImplConvertBitmap.java
@@ -0,0 +1,455 @@
+package boofcv.android;
+
+import java.lang.reflect.Method;
+import java.nio.ByteBuffer;
+import java.nio.IntBuffer;
+
+import android.graphics.Bitmap;
+import android.test.AndroidTestCase;
+import boofcv.core.image.FactoryGImageSingleBand;
+import boofcv.core.image.GImageSingleBand;
+import boofcv.core.image.GeneralizedImageOps;
+import boofcv.struct.image.ImageFloat32;
+import boofcv.struct.image.ImageSingleBand;
+import boofcv.struct.image.ImageUInt8;
+import boofcv.struct.image.MultiSpectral;
+
+public class TestImplConvertBitmap extends AndroidTestCase {
+
+	int w = 5;
+	int h = 10;
+	
+	int buffer32[] = new int[w*h];
+	byte buffer8[] = new byte[w*h*4];
+	
+	
+	public void testAll_ArrayToGray() {
+		Method methods[] = ImplConvertBitmap.class.getMethods();
+	
+		int numCount = 0;
+		
+		for( Method m : methods ) {
+			if( !m.getName().equals("arrayToGray") )
+				continue;
+		
+			checkArrayToGray(m,Bitmap.Config.ARGB_8888);
+			try {
+				checkArrayToGray(m,Bitmap.Config.RGB_565);
+			} catch( RuntimeException e ) {
+				assertFalse( "Only byte supports this mode",m.getParameterTypes()[1] == byte[].class );
+			}
+			numCount++;
+		}
+		
+		assertEquals(4,numCount);
+	}
+	
+	public void checkArrayToGray( Method m , Bitmap.Config config ) {
+		Bitmap orig = Bitmap.createBitmap(w,h, config);
+		orig.setPixel(1, 2, 0xFF204010 );
+		
+		Class[] params = m.getParameterTypes();
+		
+		try {
+			ImageSingleBand found = (ImageSingleBand)params[2].getConstructor(int.class,int.class).newInstance(w,h);
+
+			Object array;
+
+			String info = params[2].getSimpleName();
+			if (params[0] == int[].class) {
+				info += " Array32";
+				orig.copyPixelsToBuffer(IntBuffer.wrap(buffer32));
+				array = buffer32;
+			} else {
+				info += " Array8";
+				orig.copyPixelsToBuffer(ByteBuffer.wrap(buffer8));
+				array = buffer8;
+			}
+			info += " "+config;
+
+			m.invoke(null, array, config, found);
+			
+			GImageSingleBand g = FactoryGImageSingleBand.wrap(found);
+			
+			// should be 37 for both 8888 and 565
+			assertEquals(info,37,g.get(1,2).intValue());
+			assertEquals(info,0,g.get(0,0).intValue());
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+	public void testAll_ArrayToMulti() {
+		Method methods[] = ImplConvertBitmap.class.getMethods();
+	
+		int numCount = 0;
+		
+		for( Method m : methods ) {
+			if( !m.getName().contains("arrayToMulti") )
+				continue;
+		
+			checkArrayToMulti(m,Bitmap.Config.ARGB_8888);
+			try {
+				checkArrayToMulti(m,Bitmap.Config.RGB_565);
+			} catch( RuntimeException e ) {
+				assertFalse( "Only byte supports this mode",m.getParameterTypes()[1] == byte[].class );
+			}
+			numCount++;
+		}
+		
+		assertEquals(4,numCount);
+	}
+	
+	public void checkArrayToMulti( Method m , Bitmap.Config config ) {
+		Bitmap orig = Bitmap.createBitmap(w,h, config);
+		orig.setPixel(1, 2, 0xFF204010 );
+		
+		Class[] params = m.getParameterTypes();
+		
+		try {
+			int numBands = Bitmap.Config.ARGB_8888 == config ? 4 : 3;
+			Class msType = m.getName().contains("U8") ? ImageUInt8.class : ImageFloat32.class;
+			
+			MultiSpectral found = new MultiSpectral(msType,w,h,numBands);
+
+			Object array;
+
+			String info = params[2].getSimpleName();
+			if (params[0] == int[].class) {
+				info += " Array32";
+				orig.copyPixelsToBuffer(IntBuffer.wrap(buffer32));
+				array = buffer32;
+			} else {
+				info += " Array8";
+				orig.copyPixelsToBuffer(ByteBuffer.wrap(buffer8));
+				array = buffer8;
+			}
+			info += " "+config;
+
+			m.invoke(null, array, config, found);
+			
+			assertEquals(0x20,(int)GeneralizedImageOps.get(found.getBand(0),1,2));
+			assertEquals(0x40,(int)GeneralizedImageOps.get(found.getBand(1),1,2));
+			assertEquals(0x10,(int)GeneralizedImageOps.get(found.getBand(2),1,2));
+			if( numBands == 4 )
+				assertEquals(0xFF,(int)GeneralizedImageOps.get(found.getBand(3),1,2));
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+	public void testAll_BitmapToGrayRGB() {
+		Method methods[] = ImplConvertBitmap.class.getMethods();
+	
+		int numCount = 0;
+		
+		for( Method m : methods ) {
+			if( !m.getName().equals("bitmapToGrayRGB") )
+				continue;
+		
+			checkBitmapToGrayRGB(m,Bitmap.Config.ARGB_8888);
+			// the 565 conversion Android uses is slightly different from my conversion.  just going to ignore this test
+//			checkBitmapToGrayRGB(m,Bitmap.Config.RGB_565);
+			
+			numCount++;
+		}
+		
+		assertEquals(2,numCount);
+	}
+	
+	public void checkBitmapToGrayRGB( Method m , Bitmap.Config config ) {
+		Bitmap orig = Bitmap.createBitmap(w,h, config);
+		orig.setPixel(1, 2, 0xFF204010 );
+		
+		Class[] params = m.getParameterTypes();
+		
+		String info = config+" "+params[1].getSimpleName();
+		
+		try {
+			ImageSingleBand found = (ImageSingleBand)params[1].getConstructor(int.class,int.class).newInstance(w,h);
+
+			m.invoke(null, orig, found);
+			
+			GImageSingleBand g = FactoryGImageSingleBand.wrap(found);
+			
+			// should be 37 for both 8888 and 565
+			assertEquals(info,37,g.get(1,2).intValue());
+			assertEquals(info,0,g.get(0,0).intValue());
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+	public void testAll_BitmapToMultiRGB() {
+		Method methods[] = ImplConvertBitmap.class.getMethods();
+	
+		int numCount = 0;
+		
+		for( Method m : methods ) {
+			if( !m.getName().contains("bitmapToMulti") )
+				continue;
+		
+			checkBitmapToMultiRGB(m,Bitmap.Config.ARGB_8888);
+
+			numCount++;
+		}
+		
+		assertEquals(2,numCount);
+	}
+	
+	public void checkBitmapToMultiRGB( Method m , Bitmap.Config config ) {
+		Bitmap orig = Bitmap.createBitmap(w,h, config);
+		orig.setPixel(1, 2, 0xFF204010 );
+		
+		Class[] params = m.getParameterTypes();
+		
+		String info = config+" "+m.getName();
+		
+		try {
+			int numBands = Bitmap.Config.ARGB_8888 == config ? 4 : 3;
+			Class msType = m.getName().contains("U8") ? ImageUInt8.class : ImageFloat32.class;
+			
+			MultiSpectral found = new MultiSpectral(msType,w,h,numBands);
+
+			m.invoke(null, orig, found);
+			
+			assertEquals(0x20,(int)GeneralizedImageOps.get(found.getBand(0),1,2));
+			assertEquals(0x40,(int)GeneralizedImageOps.get(found.getBand(1),1,2));
+			assertEquals(0x10,(int)GeneralizedImageOps.get(found.getBand(2),1,2));
+			if( numBands == 4 )
+				assertEquals(0xFF,(int)GeneralizedImageOps.get(found.getBand(3),1,2));
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+	public void testAll_GrayToArray() {
+		Method methods[] = ImplConvertBitmap.class.getMethods();
+	
+		int numCount = 0;
+		
+		for( Method m : methods ) {
+			if( !m.getName().equals("grayToArray") )
+				continue;
+		
+			checkGrayToArray(m,Bitmap.Config.ARGB_8888);
+			checkGrayToArray(m,Bitmap.Config.RGB_565);
+			
+			numCount++;
+		}
+		
+		assertEquals(2,numCount);
+	}
+	
+	public void checkGrayToArray( Method m , Bitmap.Config config ) {
+		Bitmap dst = Bitmap.createBitmap(w,h, config);
+		
+		Class[] params = m.getParameterTypes();
+		
+		try {
+			ImageSingleBand src = (ImageSingleBand)params[0].getConstructor(int.class,int.class).newInstance(w,h);
+			GeneralizedImageOps.set(src, 1, 2, 16);
+			GeneralizedImageOps.set(src, 1, 3, 0xFF);
+			
+			Object array;
+
+			String info = params[0].getSimpleName();
+			if (params[2] == int[].class) {
+				info += " Array32";
+				array = buffer32;
+			} else {
+				info += " Array8";
+				array = buffer8;
+			}
+			info += " "+config;
+
+			m.invoke(null, src, array, config);
+			if (params[2] == int[].class) {
+				dst.copyPixelsFromBuffer(IntBuffer.wrap(buffer32));
+			} else {
+				dst.copyPixelsFromBuffer(ByteBuffer.wrap(buffer8));
+			}
+			
+			GImageSingleBand g = FactoryGImageSingleBand.wrap(src);
+			
+			if( config == Bitmap.Config.ARGB_8888 ) {
+				assertEquals(info,0xFF101010,(int)dst.getPixel(1,2));
+				assertEquals(info,0xFFFFFFFF,(int)dst.getPixel(1,3));
+			} else {
+				assertEquals(info,expected565(16,16,16),(int)dst.getPixel(1,2));
+				assertEquals(info,expected565(255,255,255),(int)dst.getPixel(1,3));
+			}
+			assertEquals(info,0xFF000000,dst.getPixel(0,0));
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+	public void testAll_GrayToBitmapRGB() {
+		Method methods[] = ImplConvertBitmap.class.getMethods();
+	
+		int numCount = 0;
+		
+		for( Method m : methods ) {
+			if( !m.getName().equals("grayToBitmapRGB") )
+				continue;
+		
+			checkGrayToBitmapRGB(m,Bitmap.Config.ARGB_8888);
+//			checkGrayToBitmapRGB(m,Bitmap.Config.RGB_565);
+			
+			numCount++;
+		}
+		
+		assertEquals(2,numCount);
+	}
+	
+	public void checkGrayToBitmapRGB( Method m , Bitmap.Config config ) {
+		Bitmap dst = Bitmap.createBitmap(w,h, config);
+		
+		Class[] params = m.getParameterTypes();
+		
+		try {
+			ImageSingleBand src = (ImageSingleBand)params[0].getConstructor(int.class,int.class).newInstance(w,h);
+			GeneralizedImageOps.set(src, 1, 2, 16);
+			GeneralizedImageOps.set(src, 1, 3, 0xFF);
+
+			String info = config+" "+params[0].getSimpleName();
+
+			m.invoke(null, src, dst);
+
+			assertEquals(info,0xFF101010,(int)dst.getPixel(1,2));
+			assertEquals(info,0xFFFFFFFF,(int)dst.getPixel(1,3));
+			assertEquals(info,0xFF000000,dst.getPixel(0,0));
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+	public void testAll_MultiToArray() {
+		Method methods[] = ImplConvertBitmap.class.getMethods();
+	
+		int numCount = 0;
+		
+		for( Method m : methods ) {
+			if( !m.getName().contains("multiToArray") )
+				continue;
+		
+			checkMultiToArray(m,Bitmap.Config.ARGB_8888);
+			checkMultiToArray(m,Bitmap.Config.RGB_565);
+			
+			numCount++;
+		}
+		
+		assertEquals(2,numCount);
+	}
+	
+	public void checkMultiToArray( Method m , Bitmap.Config config ) {
+		Bitmap dst = Bitmap.createBitmap(w,h, config);
+		
+		Class[] params = m.getParameterTypes();
+		
+		try {
+			int numBands = Bitmap.Config.ARGB_8888 == config ? 4 : 3;
+			Class msType = m.getName().contains("U8") ? ImageUInt8.class : ImageFloat32.class;
+			
+			MultiSpectral src = new MultiSpectral(msType,w,h,numBands);
+			
+			GeneralizedImageOps.set(src.getBand(0),1, 2, 0x38);
+			GeneralizedImageOps.set(src.getBand(1),1, 2, 0x64);
+			GeneralizedImageOps.set(src.getBand(2),1, 2, 0xFF);
+			if( numBands == 4 )
+				GeneralizedImageOps.set(src.getBand(3),1, 2, 0xFF);
+			
+			Object array;
+
+			String info = params[0].getSimpleName();
+			if (params[2] == int[].class) {
+				info += " Array32";
+				array = buffer32;
+			} else {
+				info += " Array8";
+				array = buffer8;
+			}
+			info += " "+config;
+
+			m.invoke(null, src, array, config);
+			if (params[2] == int[].class) {
+				dst.copyPixelsFromBuffer(IntBuffer.wrap(buffer32));
+			} else {
+				dst.copyPixelsFromBuffer(ByteBuffer.wrap(buffer8));
+			}
+
+			if( config == Bitmap.Config.ARGB_8888 ) {
+				assertEquals(info,0xFF3864FF,(int)dst.getPixel(1,2));
+				assertEquals(info,0x00000000,dst.getPixel(0,0));
+			} else {
+				assertEquals(info,expected565(0x38,0x64,0xFF),(int)dst.getPixel(1,2));
+				assertEquals(info,0xFF000000,dst.getPixel(0,0));
+			}
+			
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+	public void testAll_MultiToBitmapRGB() {
+		Method methods[] = ImplConvertBitmap.class.getMethods();
+	
+		int numCount = 0;
+		
+		for( Method m : methods ) {
+			if( !m.getName().contains("multiToBitmapRGB") )
+				continue;
+		
+			checkMultiToBitmapRGB(m,Bitmap.Config.ARGB_8888);
+//			checkMultiToBitmapRGB(m,Bitmap.Config.RGB_565);
+			
+			numCount++;
+		}
+		
+		assertEquals(2,numCount);
+	}
+	
+	public void checkMultiToBitmapRGB( Method m , Bitmap.Config config ) {
+		Bitmap dst = Bitmap.createBitmap(w,h, config);
+		
+		Class[] params = m.getParameterTypes();
+		
+		try {
+			int numBands = Bitmap.Config.ARGB_8888 == config ? 4 : 3;
+			Class msType = m.getName().contains("U8") ? ImageUInt8.class : ImageFloat32.class;
+			
+			MultiSpectral src = new MultiSpectral(msType,w,h,numBands);
+			
+			GeneralizedImageOps.set(src.getBand(0),1, 2, 0x38);
+			GeneralizedImageOps.set(src.getBand(1),1, 2, 0x64);
+			GeneralizedImageOps.set(src.getBand(2),1, 2, 0xFF);
+			if( numBands == 4 )
+				GeneralizedImageOps.set(src.getBand(3),1, 2, 0xFF);
+			
+
+			String info = m.getName()+" "+config;
+
+			m.invoke(null, src, dst);
+
+			if( config == Bitmap.Config.ARGB_8888 ) {
+				assertEquals(info,0xFF3864FF,(int)dst.getPixel(1,2));
+				assertEquals(info,0x00000000,dst.getPixel(0,0));
+			} else {
+				assertEquals(info,expected565(0x38,0x64,0xFF),(int)dst.getPixel(1,2));
+				assertEquals(info,0xFF000000,dst.getPixel(0,0));
+			}
+			
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	private static int expected565( int r , int g , int b ) {
+		int valR = (int)Math.round(r*0x1F/255.0)*0xFF/0x1F;
+		int valG = (int)Math.round(g*0x3F/255.0)*0xFF/0x3F;
+		int valB = (int)Math.round(b*0x1F/255.0)*0xFF/0x1F;
+		
+		return (0xFF << 24) | (valR << 16) | (valG << 8) | valB;
+	}
+}
