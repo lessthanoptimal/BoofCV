e752ff6cc25d020ae5390df64c0a84c1d1e462e2
Peter Abeles <peter.abeles@gmail.com>
2012-11-13 19:55:26 -0500

    - This should have been included in the last commit

---
+++ main/sfm/src/boofcv/alg/sfm/Point2D3DTrack.java
@@ -0,0 +1,15 @@
+package boofcv.alg.sfm;
+
+import boofcv.struct.geo.Point2D3D;
+
+/**
+ * Adds track maintenance information for {@link Point2D3D}.
+ * 
+ * @author Peter Abeles
+ */
+public class Point2D3DTrack extends Point2D3D {
+
+	// the tick in which it was last an inlier
+	long lastInlier;
+
+}
