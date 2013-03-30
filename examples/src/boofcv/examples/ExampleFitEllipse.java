8540c2b25605dd4151e56e080f184db65647d2c9
Peter Abeles <peter.abeles@gmail.com>
2013-03-29 22:34:24 -0400

    - Finishing touches on fitting ellipses

---
+++ examples/src/boofcv/examples/ExampleFitEllipse.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright (c) 2011-2013, Peter Abeles. All Rights Reserved.
+ *
+ * This file is part of BoofCV (http://boofcv.org).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package boofcv.examples;
+
+import boofcv.alg.feature.shapes.FitData;
+import boofcv.alg.feature.shapes.ShapeFittingOps;
+import boofcv.alg.filter.binary.BinaryImageOps;
+import boofcv.alg.filter.binary.Contour;
+import boofcv.alg.filter.binary.ThresholdImageOps;
+import boofcv.alg.misc.ImageStatistics;
+import boofcv.core.image.ConvertBufferedImage;
+import boofcv.gui.feature.VisualizeShapes;
+import boofcv.gui.image.ShowImages;
+import boofcv.io.image.UtilImageIO;
+import boofcv.struct.image.ImageFloat32;
+import boofcv.struct.image.ImageUInt8;
+import georegression.struct.shapes.EllipseRotated_F64;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.util.List;
+
+/**
+ * Demonstration which fits ellipses to object contours in a binary image.
+ *
+ * @author Peter Abeles
+ */
+public class ExampleFitEllipse {
+
+	public static void main( String args[] ) {
+		// load and convert the image into a usable format
+		BufferedImage image = UtilImageIO.loadImage("../data/applet/particles01.jpg");
+		ImageFloat32 input = ConvertBufferedImage.convertFromSingle(image, null, ImageFloat32.class);
+
+		ImageUInt8 binary = new ImageUInt8(input.width,input.height);
+
+		// the mean pixel value is often a reasonable threshold when creating a binary image
+		double mean = ImageStatistics.mean(input);
+
+		// create a binary image by thresholding
+		ThresholdImageOps.threshold(input, binary, (float) mean, true);
+
+		// reduce noise with some filtering
+		ImageUInt8 filtered = BinaryImageOps.erode8(binary,null);
+		filtered = BinaryImageOps.dilate8(filtered, null);
+
+		// Find the contour around the shapes
+		List<Contour> contours = BinaryImageOps.contour(filtered,8,null);
+
+		// Fit an ellipse to each external contour and draw the results
+		Graphics2D g2 = image.createGraphics();
+		g2.setStroke(new BasicStroke(3));
+		g2.setColor(Color.RED);
+
+		for( Contour c : contours ) {
+			FitData<EllipseRotated_F64> ellipse = ShapeFittingOps.fitEllipse_I32(c.external,0,false,null);
+			VisualizeShapes.drawEllipse(ellipse.shape, g2);
+		}
+
+//		ShowImages.showWindow(VisualizeBinaryData.renderBinary(filtered,null),"Binary");
+		ShowImages.showWindow(image,"Ellipses");
+	}
+}
