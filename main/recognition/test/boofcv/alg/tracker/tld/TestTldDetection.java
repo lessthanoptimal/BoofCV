3e7853958d1a24031ab93546cfea16ad20d5fff5
Peter Abeles <peter.abeles@gmail.com>
2013-08-28 11:17:57 -0400

    - lots of unit tests.  Basically everything passes now

--- main/recognition/test/boofcv/alg/tracker/tld/TestTldDetection.java
+++ main/recognition/test/boofcv/alg/tracker/tld/TestTldDetection.java
@@ -1,8 +1,11 @@
 package boofcv.alg.tracker.tld;
 
+import boofcv.struct.ImageRectangle;
+import boofcv.struct.image.ImageUInt8;
 import org.junit.Test;
 
-import static org.junit.Assert.fail;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 /**
  * @author Peter Abeles
@@ -10,8 +13,104 @@ import static org.junit.Assert.fail;
 public class TestTldDetection {
 
 	@Test
-	public void stuff() {
-		fail("Implement");
+	public void computeTemplateConfidence() {
+		TldDetection<ImageUInt8> alg = new TldDetection<ImageUInt8>();
+		alg.config = new TldConfig();
+		alg.config.confidenceThresholdUpper = 0.6;
+		alg.template = new HelperTemplate();
+
+		for( int i = 0; i < 4; i++ ) {
+			alg.fernRegions.add(new ImageRectangle(i,i,i,i));
+		}
+
+		alg.computeTemplateConfidence();
+
+		assertEquals(3,alg.candidateDetections.size());
+
+		for( int i = 0; i < 3; i++ ) {
+			TldRegion r = alg.candidateDetections.get(i);
+			assertEquals(0,r.connections);
+			assertTrue(r.confidence>0.6);
+			assertTrue(r.rect.x0 != 0);
+		}
+	}
+
+	/**
+	 * See if regions with larger N than P are filtered out
+	 */
+	@Test
+	public void selectBestRegionsFern_largerN() {
+		TldDetection<ImageUInt8> alg = new TldDetection<ImageUInt8>();
+		alg.config = new TldConfig();
+		alg.config.maximumCascadeConsider = 20;
+
+		// check eliminate ones which are more N than P
+		for( int i = 0; i < 10; i++ ) {
+			alg.fernInfo.grow();
+			alg.fernInfo.get(i).sumP = 6;
+			alg.fernInfo.get(i).sumN = 6;
+		}
+
+		// larger max N will make its likelihood smaller
+		alg.selectBestRegionsFern(300,200);
+
+		assertEquals(0,alg.fernRegions.size());
+	}
+
+	/**
+	 * See if the case where there are fewer than the maximum number of regions is handled correctly
+	 */
+	@Test
+	public void selectBestRegionsFern_smaller() {
+		TldDetection<ImageUInt8> alg = new TldDetection<ImageUInt8>();
+		alg.config = new TldConfig();
+		alg.config.maximumCascadeConsider = 20;
+
+		// all 10 should be accepted
+		for( int i = 0; i < 10; i++ ) {
+			alg.fernInfo.grow();
+			alg.fernInfo.get(i).r = new ImageRectangle(i,i,i,i);
+			alg.fernInfo.get(i).sumP = 20;
+			alg.fernInfo.get(i).sumN = 6;
+		}
+		alg.selectBestRegionsFern(200,200);
+
+		assertEquals(10,alg.fernRegions.size());
+	}
+
+	/**
+	 * See if the case where there are more than the maximum number of regions is handled correctly
+	 */
+	@Test
+	public void selectBestRegionsFern_larger() {
+		TldDetection<ImageUInt8> alg = new TldDetection<ImageUInt8>();
+		alg.config = new TldConfig();
+		alg.config.maximumCascadeConsider = 20;
+
+		// all 10 should be accepted
+		for( int i = 0; i < 30; i++ ) {
+			alg.fernInfo.grow();
+			alg.fernInfo.get(i).r = new ImageRectangle(i,i,i,i);
+			alg.fernInfo.get(i).sumP = 50-i;
+			alg.fernInfo.get(i).sumN = 6;
+		}
+		alg.selectBestRegionsFern(200,200);
+
+		assertEquals(20,alg.fernRegions.size());
+		// should contain all the best ones
+		for( int i = 0; i < 20; i++ ) {
+			assertTrue(alg.fernRegions.contains(alg.fernInfo.get(i).r));
+		}
+	}
+
+	protected static class HelperTemplate extends TldTemplateMatching {
+
+		int numCalled = 0;
+
+		@Override
+		public double computeConfidence( ImageRectangle r ) {
+			return 0.55 + (numCalled++)*0.1;
+		}
 	}
 
 }
--- main/recognition/test/boofcv/alg/tracker/tld/TestTldFernClassifier.java
+++ main/recognition/test/boofcv/alg/tracker/tld/TestTldFernClassifier.java
@@ -10,8 +10,8 @@ import org.junit.Test;
 
 import java.util.Random;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
 
 /**
  * @author Peter Abeles
@@ -20,6 +20,9 @@ public class TestTldFernClassifier {
 
 	int width = 60;
 	int height = 80;
+
+	int numFerns = 5;
+	int numLearnRandom = 7;
 
 	Random rand = new Random(234);
 
@@ -33,39 +36,52 @@ public class TestTldFernClassifier {
 	}
 
 	@Test
-	public void updateFerns() {
-		fail("update");
-//		CheckUpdateFernsClass alg = new CheckUpdateFernsClass();
-//
-//		alg.learnFernNoise(true, new ImageRectangle());
-//
-//		for( int i = 0; i < alg.managers.length; i++ ) {
-//			TldFernFeature f =  alg.managers[i].table[i];
-//			assertTrue(f != null);
-//			assertEquals(1, f.numP);
-//			assertEquals(0, f.numN);
-//		}
-//
-//		alg.num = 0;
-//		alg.learnFernNoise(false, new ImageRectangle());
-//
-//		for( int i = 0; i < alg.managers.length; i++ ) {
-//			TldFernFeature f =  alg.managers[i].table[i];
-//			assertTrue(f != null);
-//			assertEquals(1, f.numP);
-//			assertEquals(1, f.numN);
-//		}
+	public void learnFern() {
+		TldFernClassifier<ImageUInt8> alg = createAlg();
+
+		alg.setImage(input);
+		alg.learnFern(true, new ImageRectangle(10,12,30,45));
+
+		for( int i = 0; i < alg.managers.length; i++ ) {
+			assertEquals(1, countNum(true,alg.managers[i]));
+			assertEquals(0, countNum(false,alg.managers[i]));
+		}
+		assertTrue(alg.getMaxP() > 0 );
+		assertTrue(alg.getMaxN() == 0 );
+
+		alg.learnFern(false, new ImageRectangle(10,12,30,45));
+
+		for( int i = 0; i < alg.managers.length; i++ ) {
+			assertEquals(1, countNum(true,alg.managers[i]));
+			assertEquals(1, countNum(false,alg.managers[i]));
+		}
+		assertTrue(alg.getMaxP() > 0 );
+		assertTrue(alg.getMaxN() > 0 );
 	}
 
 	@Test
-	public void performTest() {
-		fail("update");
-//		CheckUpdateFernsClass alg = new CheckUpdateFernsClass();
-//
-//		alg.learnFernNoise(true, new ImageRectangle());
-//
-//		alg.num = 0;
-//		assertTrue(alg.performTest(new ImageRectangle()));
+	public void learnFernNoise() {
+		TldFernClassifier<ImageUInt8> alg = createAlg();
+
+		alg.setImage(input);
+		alg.learnFernNoise(true, new ImageRectangle(10,12,30,45));
+
+		for( int i = 0; i < alg.managers.length; i++ ) {
+			int found = countNum(true,alg.managers[i]);
+			assertEquals(1+numLearnRandom, found);
+			assertEquals(0, countNum(false,alg.managers[i]));
+		}
+		assertTrue(alg.getMaxP() > 0 );
+		assertTrue(alg.getMaxN() == 0 );
+
+		alg.learnFernNoise(false, new ImageRectangle(10,12,30,45));
+
+		for( int i = 0; i < alg.managers.length; i++ ) {
+			assertEquals(1+numLearnRandom, countNum(true,alg.managers[i]));
+			assertEquals(1+numLearnRandom, countNum(false,alg.managers[i]));
+		}
+		assertTrue(alg.getMaxP() > 0 );
+		assertTrue(alg.getMaxN() > 0 );
 	}
 
 	@Test
@@ -75,13 +91,18 @@ public class TestTldFernClassifier {
 
 		ImageRectangle r = new ImageRectangle(2,20,12,28);
 
+		float cx = r.x0 + r.getWidth()/2.0f;
+		float cy = r.x0 + r.getHeight()/2.0f;
+		float w = r.getWidth();
+		float h = r.getHeight();
+
 		boolean expected[] = new boolean[10];
 		for( int i = 0; i < 10; i++ ) {
 			Point2D_F32 a = fern.pairs[i].a;
 			Point2D_F32 b = fern.pairs[i].b;
 
-			float valA = interpolate.get(r.x0 + a.x*(r.x1-r.x0), r.y0 + a.y*(r.y1-r.y0));
-			float valB = interpolate.get(r.x0 + b.x*(r.x1-r.x0), r.y0 + b.y*(r.y1-r.y0));
+			float valA = interpolate.get(cx + a.x*w, cy + a.y*h);
+			float valB = interpolate.get(cx + b.x*w, cy + b.y*h);
 
 			expected[9-i] = valA < valB;
 		}
@@ -89,32 +110,101 @@ public class TestTldFernClassifier {
 		TldFernClassifier<ImageUInt8> alg = createAlg();
 		alg.setImage(input);
 
-		int found = alg.computeFernValue(r.x0,r.y0,r.getWidth(),r.getHeight(),fern);
+		int found = alg.computeFernValue(cx,cy,w,h,fern);
 
 		for( int i = 0; i < 10; i++ ) {
 			assertTrue(expected[i] == (((found >> i) & 0x0001) == 1));
 		}
+	}
 
+	@Test
+	public void computeFernValueRand() {
+		TldFernDescription fern = new TldFernDescription(rand,10);
+
+		ImageRectangle r = new ImageRectangle(2,20,12,28);
+
+		float cx = r.x0 + r.getWidth()/2.0f;
+		float cy = r.x0 + r.getHeight()/2.0f;
+		float w = r.getWidth();
+		float h = r.getHeight();
+
+		boolean expected[] = new boolean[10];
+		for( int i = 0; i < 10; i++ ) {
+			Point2D_F32 a = fern.pairs[i].a;
+			Point2D_F32 b = fern.pairs[i].b;
+
+			float valA = interpolate.get(cx + a.x*w, cy + a.y*h);
+			float valB = interpolate.get(cx + b.x*w, cy + b.y*h);
+
+			expected[9-i] = valA < valB;
+		}
+
+		TldFernClassifier<ImageUInt8> alg = createAlg();
+		alg.setImage(input);
+
+		int found = alg.computeFernValueRand(cx,cy,w,h,fern);
+
+		int numDiff = 0;
+		for( int i = 0; i < 10; i++ ) {
+			if(expected[i] != (((found >> i) & 0x0001) == 1)) {
+				numDiff++;
+			}
+		}
+
+		assertTrue(numDiff != 0 );
+		assertTrue( numDiff < 10 );
+	}
+
+		@Test
+	public void renormalizeP() {
+			TldFernClassifier<ImageUInt8> alg = createAlg();
+
+			alg.maxP = 1000;
+			alg.managers[2].table[1] = new TldFernFeature();
+			alg.managers[2].table[1].numP = 600;
+
+			alg.renormalizeP();
+
+			int expected = 600/20;
+
+			assertEquals(expected,alg.managers[2].table[1].numP);
+	}
+
+	@Test
+	public void renormalizeN() {
+		TldFernClassifier<ImageUInt8> alg = createAlg();
+
+		alg.maxN = 1000;
+		alg.managers[2].table[1] = new TldFernFeature();
+		alg.managers[2].table[1].numN = 600;
+
+		alg.renormalizeN();
+
+		int expected = 600/20;
+
+		assertEquals(expected,alg.managers[2].table[1].numN);
 	}
 
 	private TldFernClassifier<ImageUInt8> createAlg() {
-		fail("update");
-//		InterpolatePixel<ImageUInt8> interpolate = FactoryInterpolation.bilinearPixel(ImageUInt8.class);
-//		return new TldFernClassifier<ImageUInt8>(rand,10,8,interpolate);
-		return null;
+		InterpolatePixel<ImageUInt8> interpolate = FactoryInterpolation.bilinearPixel(ImageUInt8.class);
+		return new TldFernClassifier<ImageUInt8>(rand,numFerns,8,numLearnRandom,10,interpolate);
 	}
 
-//	private class CheckUpdateFernsClass extends TldFernClassifier {
-//
-//		int num = 0;
-//
-//		private CheckUpdateFernsClass() {
-//			super(rand,10,8,null);
-//		}
-//
-//		@Override
-//		protected int computeFernValue(float x0, float y0, float rectWidth, float rectHeight, TldFernDescription fern) {
-//			return num++;
-//		}
-//	}
+	private int countNum( boolean positive , TldFernManager manager ) {
+		int total = 0;
+
+		for( int i = 0; i < manager.table.length; i++ ) {
+
+			TldFernFeature f = manager.table[i];
+
+			if( f != null ) {
+				if( positive )
+					total += f.numP;
+				else
+					total += f.numN;
+			}
+		}
+
+		return total;
+	}
 }
