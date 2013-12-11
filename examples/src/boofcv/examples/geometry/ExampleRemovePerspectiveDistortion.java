e4fa6767725753d59edaea193526e0dc8798c789
Peter Abeles <peter.abeles@gmail.com>
2013-12-10 21:11:27 -0500

    - Example showing how perspective distortion is removed

--- examples/src/boofcv/examples/geometry/ExampleRemovePerspectiveDistortion.java
+++ examples/src/boofcv/examples/geometry/ExampleRemovePerspectiveDistortion.java
@@ -18,28 +18,65 @@
 
 package boofcv.examples.geometry;
 
+import boofcv.abst.geo.Estimate1ofEpipolar;
+import boofcv.alg.distort.DistortImageOps;
+import boofcv.alg.distort.PointToPixelTransform_F32;
+import boofcv.alg.distort.PointTransformHomography_F32;
+import boofcv.alg.interpolate.TypeInterpolate;
+import boofcv.core.image.ConvertBufferedImage;
+import boofcv.factory.geo.FactoryMultiView;
+import boofcv.gui.image.ShowImages;
+import boofcv.io.image.UtilImageIO;
+import boofcv.struct.distort.PixelTransform_F32;
+import boofcv.struct.geo.AssociatedPair;
+import boofcv.struct.image.ImageFloat32;
+import boofcv.struct.image.MultiSpectral;
+import georegression.struct.point.Point2D_F64;
+import org.ejml.data.DenseMatrix64F;
+
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+
 /**
+ * Certain image processing techniques, such as Optical Character Recognition (OCR), can be performed better if
+ * perspective distortion is remove from an image.  In this example a homography is computed from the cour corners
+ * of a bulletin board and the image is projected into a square image without perspective distortion.
+ *
  * @author Peter Abeles
  */
 public class ExampleRemovePerspectiveDistortion {
 	public static void main(String[] args) {
-//		WrapHomographyLinear linear = new
-//				WrapHomographyLinear(FactoryGeometryAlgs.homography4(true));
-//		ArrayList<AssociatedPair> associatedPairs = new
-//				ArrayList<AssociatedPair>();
-//		associatedPairs.add(new AssociatedPair(new Point2D_F64(pTL.x,
-//				pTL.y), new Point2D_F64(pTL_.x, pTL_.y)));
-//		associatedPairs.add(new AssociatedPair(new Point2D_F64(pBL.x,
-//				pBL.y), new Point2D_F64(pBL_.x, pBL_.y)));
-//		associatedPairs.add(new AssociatedPair(new Point2D_F64(pTR.x,
-//				pTR.y), new Point2D_F64(pTR_.x, pTR_.y)));
-//		associatedPairs.add(new AssociatedPair(new Point2D_F64(pBR.x,
-//				pBR.y), new Point2D_F64(pBR_.x, pBR_.y)));
-//		DenseMatrix64F model = new DenseMatrix64F(3,3);
-//		linear.process(associatedPairs, model);
-//
-//		ImageDistort<ImageFloat32> distort =
-//				RectifyImageOps.rectifyImage(model, ImageFloat32.class);
-//		DistortImageOps.distortMS(srcImg, dstImg, distort);
+
+		// load a color image
+		BufferedImage buffered = UtilImageIO.loadImage("/home/pja/Desktop/goals_and_stuff.jpg");
+		MultiSpectral<ImageFloat32> input = ConvertBufferedImage.convertFromMulti(buffered, null, true, ImageFloat32.class);
+
+		// Create a smaller output image for processing later on
+		MultiSpectral<ImageFloat32> output = input._createNew(400,500);
+
+		// create homography estimation algorithm.  Requests a minimum of 4 points
+		Estimate1ofEpipolar computeHomography = FactoryMultiView.computeHomography(true);
+
+		// Specify the pixel coordinates from destination to target
+		ArrayList<AssociatedPair> associatedPairs = new ArrayList<AssociatedPair>();
+		associatedPairs.add(new AssociatedPair(new Point2D_F64(0,0),new Point2D_F64(267,182)));
+		associatedPairs.add(new AssociatedPair(new Point2D_F64(output.width-1,0),new Point2D_F64(542,68)));
+		associatedPairs.add(new AssociatedPair(new Point2D_F64(output.width-1,output.height-1),new Point2D_F64(519,736)));
+		associatedPairs.add(new AssociatedPair(new Point2D_F64(0,output.height-1),new Point2D_F64(276,570)));
+
+		// Compute the homography
+		DenseMatrix64F H = new DenseMatrix64F(3,3);
+		computeHomography.process(associatedPairs, H);
+
+		// Create the transform for distorting the image
+		PointTransformHomography_F32 homography = new PointTransformHomography_F32(H);
+		PixelTransform_F32 pixelTransform = new PointToPixelTransform_F32(homography);
+
+		// Apply distortion and show the results
+		DistortImageOps.distortMS(input,output,pixelTransform,true, TypeInterpolate.BILINEAR);
+
+		BufferedImage flat = ConvertBufferedImage.convertTo_F32(output,null,true);
+		ShowImages.showWindow(buffered,"Original Image");
+		ShowImages.showWindow(flat,"Without Perspective Distortion");
 	}
 }
--- examples/src/boofcv/examples/tracking/ExampleTrackerMeanShift.java
+++ examples/src/boofcv/examples/tracking/ExampleTrackerMeanShift.java
@@ -37,9 +37,9 @@ import java.awt.image.BufferedImage;
 
 /**
  * Mean-shift based trackers use color information to perform a local search.  The Comaniciu based trackers match
- * the color histogram directly and are more robust to objects with complex distributions.  Comaniciu can also
+ * the color histogram directly, are more robust to objects with complex distributions, and estimate scale changes.
  * be configured to estimate the object's scale.  Likelihood based trackers use color to compute a likelihood
- * function which mean-shift is then run on.  The likelihood based trackers are very fast but only work well
+ * function, which mean-shift is then run on.  The likelihood based trackers are very fast but only work well
  * when the target is composed of a single color.
  *
  * @author Peter Abeles
