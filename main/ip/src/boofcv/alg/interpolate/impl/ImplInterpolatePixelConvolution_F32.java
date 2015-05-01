75728d52c53312fb9552510c888f190e01b03d10
Peter Abeles <peter.abeles@gmail.com>
2015-05-01 08:54:16 -0700

    WTF it didn't commit everything

--- main/ip/src/boofcv/alg/interpolate/impl/ImplInterpolatePixelConvolution_F32.java
+++ main/ip/src/boofcv/alg/interpolate/impl/ImplInterpolatePixelConvolution_F32.java
@@ -15,6 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package boofcv.alg.interpolate.impl;
 
 import boofcv.alg.interpolate.InterpolatePixelS;
