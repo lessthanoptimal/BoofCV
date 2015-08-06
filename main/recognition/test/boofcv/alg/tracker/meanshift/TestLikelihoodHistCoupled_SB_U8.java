68215b67c09234949e5bc2528c0860ee2887a8e1
Peter Abeles <peter.abeles@gmail.com>
2015-08-05 22:43:35 -0700

    - wrote unit tests for two classes

--- main/recognition/test/boofcv/alg/tracker/meanshift/TestLikelihoodHistCoupled_SB_U8.java
+++ main/recognition/test/boofcv/alg/tracker/meanshift/TestLikelihoodHistCoupled_SB_U8.java
@@ -18,16 +18,66 @@
 
 package boofcv.alg.tracker.meanshift;
 
+import boofcv.struct.image.ImageUInt8;
+import georegression.struct.shapes.RectangleLength2D_I32;
 import org.junit.Test;
 
-import static org.junit.Assert.fail;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 /**
  * @author Peter Abeles
  */
 public class TestLikelihoodHistCoupled_SB_U8 {
+
 	@Test
-	public void foo(){
-		fail("implement");
+	public void singleColor() {
+		LikelihoodHistCoupled_SB_U8 alg = new LikelihoodHistCoupled_SB_U8(255,11);
+
+		ImageUInt8 image = new ImageUInt8(30,40);
+
+		RectangleLength2D_I32 r = new RectangleLength2D_I32(3,4,12,8);
+		setColor(image, r, 100);
+
+		alg.setImage(image);
+		alg.createModel(r);
+
+		assertEquals(1.0f,alg.compute(3,4),1e-4);
+		assertEquals(1.0f,alg.compute(14,11),1e-4);
+		assertEquals(0, alg.compute(10, 30), 1e-4);
+
+	}
+
+	@Test
+	public void multipleColors() {
+		LikelihoodHistCoupled_SB_U8 alg = new LikelihoodHistCoupled_SB_U8(255,11);
+
+		ImageUInt8 image = new ImageUInt8(30,40);
+
+		RectangleLength2D_I32 r0 = new RectangleLength2D_I32(3,4,8,8);
+		RectangleLength2D_I32 r1 = new RectangleLength2D_I32(11,4,4,8);
+		setColor(image,r0,100);
+		setColor(image,r1,50);
+
+
+		RectangleLength2D_I32 region = new RectangleLength2D_I32(3,4,12,8);
+		alg.setImage(image);
+		alg.createModel(region);
+
+
+		float v0 = alg.compute(3,4);
+		float v1 = alg.compute(11,4);
+
+		assertEquals(1.0f,v0+v1,1e-4);
+		assertTrue(v0>v1);
+	}
+
+	public static void setColor(ImageUInt8 image , RectangleLength2D_I32 rect , int value ) {
+
+		for( int y = 0; y < rect.height; y++ ) {
+			for( int x = 0; x < rect.width; x++ ) {
+				image.set(x+rect.x0,y+rect.y0,value);
+			}
+		}
 	}
 }
