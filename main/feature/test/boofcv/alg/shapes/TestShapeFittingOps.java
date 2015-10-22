f3b0d49cb7d456179edc9e21be85a5df3507a994
Peter Abeles <peter.abeles@gmail.com>
2015-10-22 11:09:33 -0700

    - fixed a couple more unit tests

--- main/feature/test/boofcv/alg/shapes/TestShapeFittingOps.java
+++ main/feature/test/boofcv/alg/shapes/TestShapeFittingOps.java
@@ -44,7 +44,7 @@ public class TestShapeFittingOps {
 	public void fitPolygon_loop() {
 		List<Point2D_I32> sequence = createRectangle();
 
-		List<PointIndex_I32> result = ShapeFittingOps.fitPolygon(sequence,true,0.05,1.0,100);
+		List<PointIndex_I32> result = ShapeFittingOps.fitPolygon(sequence,true,0.05,0,100);
 
 		assertEquals(4, result.size());
 		checkPolygon(new int[]{0, 0, 0, 9, 5, 9, 5, 0}, new int[]{0, 9, 14, 23}, result);
@@ -57,7 +57,7 @@ public class TestShapeFittingOps {
 	public void fitPolygon_regular() {
 		List<Point2D_I32> sequence = createRectangle();
 
-		List<PointIndex_I32> result = ShapeFittingOps.fitPolygon(sequence,false,0.05,1.0,100);
+		List<PointIndex_I32> result = ShapeFittingOps.fitPolygon(sequence,false,0.05,0,100);
 
 		assertEquals(5, result.size());
 		checkPolygon(new int[]{0, 0, 0, 9, 5, 9, 5, 0, 1, 0}, new int[]{0, 9, 14, 23, 27}, result);
