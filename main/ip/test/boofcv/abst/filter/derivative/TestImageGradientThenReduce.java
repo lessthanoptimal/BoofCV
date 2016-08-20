3dda73c1dbd4aba565cbc59b88aae7c0234c80a5
Peter Abeles <peter.abeles@gmail.com>
2016-08-20 08:56:08 -0700

    Unit Tests

--- main/ip/test/boofcv/abst/filter/derivative/TestImageGradientThenReduce.java
+++ main/ip/test/boofcv/abst/filter/derivative/TestImageGradientThenReduce.java
@@ -18,9 +18,14 @@
 
 package boofcv.abst.filter.derivative;
 
+import boofcv.core.image.border.BorderType;
+import boofcv.struct.image.GrayF32;
+import boofcv.struct.image.ImageType;
+import boofcv.struct.image.Planar;
 import org.junit.Test;
 
-import static org.junit.Assert.fail;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 /**
  * @author Peter Abeles
@@ -28,6 +33,77 @@ import static org.junit.Assert.fail;
 public class TestImageGradientThenReduce {
 	@Test
 	public void stuff() {
-		fail("Implement");
+
+		HelperGradient gradient = new HelperGradient();
+		HelperReduce reduce = new HelperReduce();
+
+		Planar<GrayF32> input = new Planar<GrayF32>(GrayF32.class,10,12,3);
+		GrayF32 outDerivX = new GrayF32(10,12);
+		GrayF32 outDerivY = new GrayF32(10,12);
+
+		ImageGradientThenReduce<Planar<GrayF32>,Planar<GrayF32>,GrayF32> alg =
+				new ImageGradientThenReduce<Planar<GrayF32>,Planar<GrayF32>,GrayF32>(gradient,reduce);
+
+		alg.process(input,outDerivX,outDerivY);
+
+		assertEquals(1,outDerivX.get(2,3),1e-4f);
+		assertEquals(2,outDerivY.get(2,3),1e-4f);
+
+		alg.setBorderType(BorderType.EXTENDED);
+		assertTrue(gradient.setBorderCalled);
+
+	}
+
+	public static class HelperGradient implements ImageGradient<Planar<GrayF32>,Planar<GrayF32>> {
+
+		public boolean setBorderCalled = false;
+
+		@Override
+		public void setBorderType(BorderType type) {
+			setBorderCalled = true;
+		}
+
+		@Override
+		public BorderType getBorderType() {
+			return BorderType.EXTENDED;
+		}
+
+		@Override
+		public int getBorder() {
+			return 0;
+		}
+
+		@Override
+		public ImageType<Planar<GrayF32>> getDerivativeType() {
+			return ImageType.pl(3,GrayF32.class);
+		}
+
+		@Override
+		public void process(Planar<GrayF32> inputImage, Planar<GrayF32> derivX, Planar<GrayF32> derivY) {
+			derivX.getBand(0).set(5,6,1);
+			derivY.getBand(0).set(5,6,2);
+		}
+	}
+
+	public static class HelperReduce implements GradientMultiToSingleBand<Planar<GrayF32>,GrayF32> {
+
+		@Override
+		public void process(Planar<GrayF32> inDerivX, Planar<GrayF32> inDerivY, GrayF32 outDerivX, GrayF32 outDerivY) {
+			assertEquals(1,inDerivX.getBand(0).get(5,6),1e-5f);
+			assertEquals(2,inDerivY.getBand(0).get(5,6),1e-5f);
+
+			outDerivX.set(2,3,1);
+			outDerivY.set(2,3,2);
+		}
+
+		@Override
+		public ImageType<Planar<GrayF32>> getInputType() {
+			return ImageType.pl(3,GrayF32.class);
+		}
+
+		@Override
+		public Class<GrayF32> getOutputType() {
+			return GrayF32.class;
+		}
 	}
 }
--- main/ip/test/boofcv/alg/filter/derivative/TestGradientReduceToSingle.java
+++ main/ip/test/boofcv/alg/filter/derivative/TestGradientReduceToSingle.java
@@ -18,16 +18,95 @@
 
 package boofcv.alg.filter.derivative;
 
+import boofcv.alg.misc.GImageMiscOps;
+import boofcv.struct.image.GrayF32;
+import boofcv.struct.image.GrayU8;
+import boofcv.struct.image.Planar;
 import org.junit.Test;
 
-import static org.junit.Assert.fail;
+import java.util.Random;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * @author Peter Abeles
  */
 public class TestGradientReduceToSingle {
+
+	Random rand = new Random(234);
+	int width = 20;
+	int height = 30;
+	int numbands = 3;
+
 	@Test
-	public void stuff() {
-		fail("Implement");
+	public void maxf_plf32_f32() {
+		Planar<GrayF32> inX = new Planar<GrayF32>(GrayF32.class,width,height,numbands);
+		Planar<GrayF32> inY = new Planar<GrayF32>(GrayF32.class,width,height,numbands);
+		GrayF32 outX = new GrayF32(width,height);
+		GrayF32 outY = new GrayF32(width,height);
+
+		GImageMiscOps.fillUniform(inX,rand,0,100);
+		GImageMiscOps.fillUniform(inY,rand,0,100);
+
+		GradientReduceToSingle.maxf(inX,inY,outX,outY);
+
+		for (int y = 0; y < height; y++) {
+			for (int x = 0; x < width; x++) {
+				float maxNorm = 0;
+				float maxValueX = -Float.MAX_VALUE;
+				float maxValueY = -Float.MAX_VALUE;
+
+				for (int i = 0; i < numbands; i++) {
+					float dx = inX.getBand(i).get(x,y);
+					float dy = inY.getBand(i).get(x,y);
+
+					float r = dx*dx + dy*dy;
+					if( r > maxNorm ) {
+						maxNorm = r;
+						maxValueX = dx;
+						maxValueY = dy;
+					}
+				}
+
+				assertEquals(maxValueX, outX.get(x,y), 1e-4f);
+				assertEquals(maxValueY, outY.get(x,y), 1e-4f);
+			}
+		}
+	}
+
+	@Test
+	public void maxf_plfu8_u8() {
+		Planar<GrayU8> inX = new Planar<GrayU8>(GrayU8.class,width,height,numbands);
+		Planar<GrayU8> inY = new Planar<GrayU8>(GrayU8.class,width,height,numbands);
+		GrayU8 outX = new GrayU8(width,height);
+		GrayU8 outY = new GrayU8(width,height);
+
+		GImageMiscOps.fillUniform(inX,rand,0,100);
+		GImageMiscOps.fillUniform(inY,rand,0,100);
+
+		GradientReduceToSingle.maxf(inX,inY,outX,outY);
+
+		for (int y = 0; y < height; y++) {
+			for (int x = 0; x < width; x++) {
+				int maxNorm = 0;
+				int maxValueX = -Integer.MAX_VALUE;
+				int maxValueY = -Integer.MAX_VALUE;
+
+				for (int i = 0; i < numbands; i++) {
+					int dx = inX.getBand(i).get(x,y);
+					int dy = inY.getBand(i).get(x,y);
+
+					int r = dx*dx + dy*dy;
+					if( r > maxNorm ) {
+						maxNorm = r;
+						maxValueX = dx;
+						maxValueY = dy;
+					}
+				}
+
+				assertEquals(maxValueX, outX.get(x,y), 1e-4f);
+				assertEquals(maxValueY, outY.get(x,y), 1e-4f);
+			}
+		}
 	}
 }
