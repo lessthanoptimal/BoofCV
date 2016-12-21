a5c4be1c2925d87f93620a10083405c8f5ce265f
Peter Abeles <peter.abeles@gmail.com>
2016-12-20 22:11:09 -0800

    - initial sketch of MLS

---
+++ main/ip/src/boofcv/alg/distort/mls/ImageDeformPointMLS_F64.java
@@ -0,0 +1,308 @@
+/*
+ * Copyright (c) 2011-2016, Peter Abeles. All Rights Reserved.
+ *
+ * This file is part of BoofCV (http://boofcv.org).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package boofcv.alg.distort.mls;
+
+import boofcv.struct.distort.Point2Transform2_F64;
+import georegression.struct.point.Point2D_F64;
+import org.ddogleg.struct.FastQueue;
+import org.ddogleg.struct.GrowQueue_F64;
+
+/**
+ * <p>Implementation of 'Moving Least Squares' control point based image deformation models described in [1].</p>
+ *
+ * Usage Procedure:
+ * <ol>
+ *     <li>Invoke {@link #configure}</li>
+ *     <li>Invoke {@link #addControl} for each control point</li>
+ *     <li>Invoke {@link #fixateControlPoints()} when all control points have been added</li>
+ *     <li>Invoke {@link #setDistorted} to change the distorted location of a control point</li>
+ *     <li>Invoke {@link #fixateDistorted()} after you are done changing distorted locations</li>
+ * </ol>
+ *
+ * <p>Each control point has an undistorted and distorted location.  The fixate functions are used to precompute
+ * different portions of the deformation to maximize speed by avoiding duplicate computations. Instead of computing
+ * a distortion for each pixel a regular grid is used instead.  Pixel points are interpolated between grid points
+ * using bilinear interpolation.
+ * </p>
+ *
+ * <p>[1] Schaefer, Scott, Travis McPhail, and Joe Warren. "Image deformation using moving least squares."
+ * ACM transactions on graphics (TOG). Vol. 25. No. 3. ACM, 2006.</p>
+ *
+ * @author Peter Abeles
+ */
+// TODO add similar
+// TODO add rigid
+public class ImageDeformPointMLS_F64 implements Point2Transform2_F64 {
+
+	// control points that specifiy the distortion
+	FastQueue<Control> controls = new FastQueue<>(Control.class,true);
+
+	// size of interpolation grid
+	int gridRows,gridCols;
+	// points inside interpolation grid
+	FastQueue<AffineCache> grid = new FastQueue<>(AffineCache.class, true);
+
+	// parameter used to adjust how distance between control points is weighted
+	double alpha = 3.0/2.0;
+
+	// scale between image and grid
+	double scaleX,scaleY;
+
+	/**
+	 * Specifies the input image size and the size of the grid it will use to approximate the idea solution
+	 * @param width Image width
+	 * @param height Image height
+	 * @param gridRows grid rows
+	 * @param gridCols grid columns
+	 */
+	public void configure( int width , int height , int gridRows , int gridCols ) {
+		scaleX = width/(double)gridCols;
+		scaleY = height/(double)gridRows;
+	}
+
+	public int addControl( double x , double y ) {
+		Control c = controls.grow();
+		c.p.set(x,y);
+		c.q.set(x,y);
+
+		return controls.size()-1;
+	}
+
+	public void setDistorted( int which , float x , float y ) {
+		controls.get(which).q.set(x,y);
+	}
+
+	public void fixateControlPoints() {
+
+		for (int row = 0; row < gridRows; row++) {
+			for (int col = 0; col < gridCols; col++) {
+				AffineCache cache = getGrid(row,col);
+				cache.weights.resize(controls.size);
+				cache.A.resize(controls.size);
+
+				double v_x = col;
+				double v_y = row;
+
+				computeWeights(cache.weights.data, v_x, v_y);
+				computeAverageP( cache);
+				computeAffineCache( cache, v_x, v_y);
+			}
+		}
+	}
+
+	public void fixateDistorted() {
+		for (int row = 0; row < gridRows; row++) {
+			for (int col = 0; col < gridCols; col++) {
+				AffineCache cache = getGrid(row,col);
+				computeAverageQ( cache );
+				computeDeformed( cache );
+			}
+		}
+	}
+
+	private void computeDeformed( AffineCache cache ) {
+		Point2D_F64 deformed = cache.deformed;
+		deformed.set(0,0);
+
+		int N = cache.A.size;
+		for (int i = 0; i < N; i++) {
+			Control c = controls.get(i);
+			double a = cache.A.data[i];
+			deformed.x += a*c.q.x + cache.aveQ.x;
+			deformed.y += a*c.q.y + cache.aveQ.y;
+		}
+	}
+
+
+	private void computeAffineCache(AffineCache cache, double v_x, double v_y) {
+		double[] weights = cache.weights.data;
+
+		// compute the inner 2x2 matrix
+		// sum p[i]'*w[i]*p[i]
+		double inner00 = 0, inner01 = 0, inner11 = 0;
+
+		for (int i = 0; i < controls.size(); i++) {
+			Control c = controls.get(i);
+			double w = weights[i];
+
+			inner00 += c.p.x*c.p.x*w;
+			inner01 += c.p.y*c.p.x*w;
+			inner11 += c.p.y*c.p.y*w;
+		}
+
+		// invert it using minors equation
+		double det = (inner00*inner00 - inner01*inner01);
+
+		double inv00 =  inner00 / det;
+		double inv01 = -inner01 / det;
+		double inv11 =  inner11 / det;
+
+		// Finally compute A[i] for each control point
+		for (int i = 0; i < controls.size(); i++) {
+			Control c = controls.get(i);
+
+			double v_m_ap_x = v_x - cache.aveP.x;
+			double v_m_ap_y = v_y - cache.aveP.y;
+
+			double tmp0 = v_m_ap_x * inv00 + v_m_ap_y * inv01;
+			double tmp1 = v_m_ap_x * inv01 + v_m_ap_y * inv11;
+
+			cache.A.data[i] = tmp0 * c.p.x + tmp1 * c.p.y;
+		}
+	}
+
+	private void computeAverageP(AffineCache cache) {
+		double[] weights = cache.weights.data;
+		cache.aveP.set(0,0);
+
+		for (int i = 0; i < controls.size(); i++) {
+			Control c = controls.get(i);
+			double w = weights[i];
+			cache.aveP.x = c.p.x * w;
+			cache.aveP.y = c.p.y * w;
+		}
+	}
+
+	private void computeAverageQ(AffineCache cache) {
+		double[] weights = cache.weights.data;
+		cache.aveQ.set(0,0);
+
+		for (int i = 0; i < controls.size(); i++) {
+			Control c = controls.get(i);
+			double w = weights[i];
+			cache.aveQ.x = c.q.x * w;
+			cache.aveQ.y = c.q.y * w;
+		}
+	}
+
+	private void computeWeights(double[] weights, double v_x, double v_y) {
+		// first compute the weights
+		double totalWeight = 0.0;
+		for (int i = 0; i < controls.size(); i++) {
+			Control c = controls.get(i);
+
+			double d2 = c.p.distance2(v_x, v_y);
+			// check for the special case
+			if( d2 == 0 ) {
+				for (int j = 0; j < controls.size(); j++) {
+					weights[j] = i==j ? 1.0f : 0.0;
+				}
+				totalWeight = 1.0;
+				break;
+			} else {
+				totalWeight += weights[i] = 1.0/Math.pow(d2,alpha);
+			}
+		}
+
+		// normalize the weights to reduce overflow and for the average calculation
+		for (int i = 0; i < controls.size(); i++) {
+			weights[i] /= totalWeight;
+		}
+	}
+
+	@Override
+	public void compute(double x, double y, Point2D_F64 out) {
+		computeGridCoordinate(x/scaleX, y/scaleY, out);
+		out.x *= scaleX;
+		out.y *= scaleY;
+	}
+
+	/**
+	 * Samples the 4 grid points around v and performs bilinear interpolation
+	 * @param v_x Grid coordinate x-axis, undistored
+	 * @param v_y Grid coordinate y-axis, undistorted
+	 * @param distorted Distorted grid coordinate
+	 */
+	private void computeGridCoordinate( double v_x , double v_y , Point2D_F64 distorted ) {
+
+		// sample the closest point and x+1,y+1
+		int x0 = (int)v_x;
+		int y0 = (int)v_y;
+		int x1 = x0+1;
+		int y1 = y0+1;
+
+		// make sure the 4 sample points are in bounds
+		if( x1 >= gridCols )
+			x1 = gridCols-1;
+		if( y1 >= gridCols )
+			y1 = gridCols-1;
+
+		// weight along each axis
+		double ax = v_x - x0;
+		double ay = v_y - y0;
+
+		// bilinear weight for each sample point
+		double w00 = (1.0 - ax) * (1.0 - ay);
+		double w01 = ax * (1.0 - ay);
+		double w11 = ax * ay;
+		double w10 = (1.0 - ax) * ay;
+
+		// apply weights to each sample point
+		Point2D_F64 d00 = getGrid(y0,x0).deformed;
+		Point2D_F64 d01 = getGrid(y0,x1).deformed;
+		Point2D_F64 d10 = getGrid(y1,x0).deformed;
+		Point2D_F64 d11 = getGrid(y1,x1).deformed;
+
+		distorted.set(0,0);
+		distorted.x += w00 * d00.x;
+		distorted.x += w01 * d01.x;
+		distorted.x += w11 * d11.x;
+		distorted.x += w10 * d10.x;
+
+		distorted.y += w00 * d00.y;
+		distorted.y += w01 * d01.y;
+		distorted.y += w11 * d11.y;
+		distorted.y += w10 * d10.y;
+	}
+
+	private AffineCache getGrid( int row , int col ) {
+		return grid.data[row*gridCols + col];
+	}
+
+	private static class Cache {
+		// location of the final deformed point
+		public Point2D_F64 deformed = new Point2D_F64();
+	}
+
+	private static class AffineCache extends Cache {
+		public GrowQueue_F64 weights = new GrowQueue_F64();
+		public GrowQueue_F64 A = new GrowQueue_F64();
+		public Point2D_F64 aveP = new Point2D_F64();
+		public Point2D_F64 aveQ = new Point2D_F64();
+	}
+
+	public double getAlpha() {
+		return alpha;
+	}
+
+	public void setAlpha(double alpha) {
+		this.alpha = alpha;
+	}
+
+	public static class Control {
+		/**
+		 * Control point location
+		 */
+		Point2D_F64 p = new Point2D_F64();
+		/**
+		 * Deformed control point location
+		 */
+		Point2D_F64 q = new Point2D_F64();
+	}
+}
