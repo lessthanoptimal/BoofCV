c899388124f9745b361436523db91127fab15497
Peter Abeles <peter.abeles@gmail.com>
2017-11-01 21:54:19 -0700

    unbreaking factory that was using experimental code

--- main/boofcv-feature/src/main/java/boofcv/factory/shape/FactoryShapeDetector.java
+++ main/boofcv-feature/src/main/java/boofcv/factory/shape/FactoryShapeDetector.java
@@ -104,11 +104,11 @@ public class FactoryShapeDetector {
 	{
 		config.checkValidity();
 
-//		PointsToPolyline contourToPolygon =
-//				FactoryPointsToPolyline.splitMerge(config.contourToPoly);
-
 		PointsToPolyline contourToPolygon =
-				FactoryPointsToPolyline.featuresSplitMerge(null);
+				FactoryPointsToPolyline.splitMerge(config.contourToPoly);
+
+//		PointsToPolyline contourToPolygon =
+//				FactoryPointsToPolyline.featuresSplitMerge(null);
 
 		return new DetectPolygonFromContour<>(
 				config.minimumSides, config.maximumSides, contourToPolygon,
