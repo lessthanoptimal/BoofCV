7812a645040054b4105234f7a1e7082daa584a99
Peter Abeles <peter.abeles@gmail.com>
2018-02-22 06:57:24 -0800

    - adding a function to make jni easier

--- main/boofcv-ip/src/main/java/boofcv/struct/BlockIndexLength.java
+++ main/boofcv-ip/src/main/java/boofcv/struct/BlockIndexLength.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011-2017, Peter Abeles. All Rights Reserved.
+ * Copyright (c) 2011-2018, Peter Abeles. All Rights Reserved.
  *
  * This file is part of BoofCV (http://boofcv.org).
  *
@@ -25,6 +25,12 @@ package boofcv.struct;
  */
 public class BlockIndexLength extends IndexLength {
 	public int block;
+
+	public void set( int start , int length , int block ) {
+		this.start = start;
+		this.length = length;
+		this.block = block;
+	}
 
 	@Override
 	public void zero() {
