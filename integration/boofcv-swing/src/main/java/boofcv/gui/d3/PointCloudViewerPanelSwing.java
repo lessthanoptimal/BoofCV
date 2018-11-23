2080bef7e76e408f9168973fa4897640be90f245
Peter Abeles <peter.abeles@gmail.com>
2018-11-23 06:43:26 -0800

    - fixed bug of keys repeating when focus lost in cloud viewer

--- integration/boofcv-swing/src/main/java/boofcv/gui/d3/PointCloudViewerPanelSwing.java
+++ integration/boofcv-swing/src/main/java/boofcv/gui/d3/PointCloudViewerPanelSwing.java
@@ -109,7 +109,7 @@ public class PointCloudViewerPanelSwing extends JPanel
 		addFocusListener(new FocusListener(){
 			@Override
 			public void focusGained(FocusEvent e) {
-//				System.out.println("focus gained");
+				System.out.println("focus gained");
 				KeyboardFocusManager.getCurrentKeyboardFocusManager().addKeyEventDispatcher(keyboard);
 
 				// start a timed task which checks current key presses. Less OS dependent this way
@@ -119,10 +119,11 @@ public class PointCloudViewerPanelSwing extends JPanel
 
 			@Override
 			public void focusLost(FocusEvent e) {
-//				System.out.println("focus lost");
+				System.out.println("focus lost");
 				KeyboardFocusManager.getCurrentKeyboardFocusManager().removeKeyEventDispatcher(keyboard);
 				pressedTask.shutdown();
 				pressedTask = null;
+				resetKey();
 			}
 		});
 	}
@@ -130,6 +131,11 @@ public class PointCloudViewerPanelSwing extends JPanel
 	public PointCloudViewerPanelSwing( float hfov, float keyStepSize) {
 		this(keyStepSize);
 		setHorizontalFieldOfView(hfov);
+	}
+
+	public void resetKey() {
+		pressed.clear();
+		shiftPressed = false;
 	}
 
 	synchronized public void setWorldToCamera( Se3_F32 worldToCamera ) {
