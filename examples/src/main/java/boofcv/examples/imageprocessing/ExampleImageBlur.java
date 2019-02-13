dee96709d030ab47abc060bc2e8b02c749ab06da
Peter Abeles <peter.abeles@gmail.com>
2019-02-13 02:52:43 -0800

    changing data reference and blur image

--- examples/src/main/java/boofcv/examples/imageprocessing/ExampleImageBlur.java
+++ examples/src/main/java/boofcv/examples/imageprocessing/ExampleImageBlur.java
@@ -44,7 +44,7 @@ public class ExampleImageBlur {
 
 	public static void main(String[] args) {
 		ListDisplayPanel panel = new ListDisplayPanel();
-		BufferedImage buffered = UtilImageIO.loadImage(UtilIO.pathExample("sunflowers.jpg"));
+		BufferedImage buffered = UtilImageIO.loadImage(UtilIO.pathExample("standard/kodim17.jpg"));
 
 		panel.addImage(buffered,"Original");
 
