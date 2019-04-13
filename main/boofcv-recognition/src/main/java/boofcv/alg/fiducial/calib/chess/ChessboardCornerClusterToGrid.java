053657e16cd350cd793aedac6cff92dee0fddf70
Peter Abeles <peter.abeles@gmail.com>
2019-04-13 14:29:10 -0700

    - WIP unit tests

--- main/boofcv-recognition/src/main/java/boofcv/alg/fiducial/calib/chess/ChessboardCornerClusterToGrid.java
+++ main/boofcv-recognition/src/main/java/boofcv/alg/fiducial/calib/chess/ChessboardCornerClusterToGrid.java
@@ -270,25 +270,44 @@ public class ChessboardCornerClusterToGrid {
 		return true;
 	}
 
-	private void sortEdgesCCW(FastQueue<Node> corners) {
+	/**
+	 * Sorts edges so that they point towards nodes in an increasing counter clockwise direction
+	 */
+	void sortEdgesCCW(FastQueue<Node> corners) {
 		for (int nodeIdx = 0; nodeIdx < corners.size; nodeIdx++) {
 			Node na = corners.get(nodeIdx);
 
+			int count = 0;
 			for (int i = 0; i < 4; i++) {
 				order[i] = i;
 				tmpEdges[i] = na.edges[i];
 				if( na.edges[i] == null ) {
 					directions[i] = Double.MAX_VALUE;
 				} else {
+					count++;
 					Node nb = na.edges[i];
-
-					directions[i] = Math.atan2(nb.y-na.y,nb.x-nb.x);
+					directions[i] = Math.atan2(nb.y-na.y,nb.x-na.x);
 				}
 			}
 
 			sorter.sort(directions,0,4,order);
 			for (int i = 0; i < 4; i++) {
 				na.edges[i] = tmpEdges[ order[i] ];
+			}
+			// Edges need to point along the 4 possible directions, in the case of 3 edges, there might
+			// need to be a gap at a different location than at the end
+			if( count == 3 ) {
+				double tail = UtilAngle.distanceCCW(directions[order[2]],directions[order[0]]);
+				for (int i = 1; i <3; i++) {
+					double ccw = UtilAngle.distanceCCW(directions[order[i-1]],directions[order[i]]);
+					if( tail < ccw ) {
+						for (int j = 3; j >= i+1; j--) {
+							na.edges[j] = na.edges[j-1];
+						}
+						na.edges[i] = null;
+						break;
+					}
+				}
 			}
 		}
 	}
--- main/boofcv-recognition/src/test/java/boofcv/alg/fiducial/calib/chess/TestChessboardCornerClusterToGrid.java
+++ main/boofcv-recognition/src/test/java/boofcv/alg/fiducial/calib/chess/TestChessboardCornerClusterToGrid.java
@@ -18,9 +18,12 @@
 
 package boofcv.alg.fiducial.calib.chess;
 
+import boofcv.alg.fiducial.calib.chess.ChessboardCornerGraph.Node;
+import georegression.metric.UtilAngle;
+import org.ddogleg.struct.FastQueue;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * @author Peter Abeles
@@ -59,12 +62,96 @@ public class TestChessboardCornerClusterToGrid
 
 	@Test
 	void alignEdges() {
+		// TODO write next.
 		fail("Implement");
 	}
 
 	@Test
 	void sortEdgesCCW() {
-		fail("Implement");
+		FastQueue<Node> corners = new FastQueue<>(Node.class,true);
+
+		for (int nodeIdx = 0; nodeIdx < 6; nodeIdx++) {
+			Node target = corners.grow();
+			target.x = 10;
+			target.y = 12;
+
+			for (int i = 0; i < 3; i++) {
+				double theta = -(i+nodeIdx)*0.5+nodeIdx;
+				double c = Math.cos(theta);
+				double s = Math.sin(theta);
+
+				double r = 4;
+				Node n = new Node();
+				n.x = target.x + r*c;
+				n.y = target.y + r*s;
+				target.edges[i+1] = n;
+			}
+		}
+
+		ChessboardCornerClusterToGrid alg = new ChessboardCornerClusterToGrid();
+
+		alg.sortEdgesCCW(corners);
+
+		for( Node n : corners.toList() ) {
+			for (int i = 1; i < 3; i++) {
+				Node m0 = n.edges[i-1];
+				Node m1 = n.edges[i];
+
+				double theta0 = Math.atan2(m0.y-n.y,m0.x-n.x);
+				double theta1 = Math.atan2(m1.y-n.y,m1.x-n.x);
+
+				double diff = UtilAngle.minus(theta1,theta0);
+				assertTrue(diff>0.1);
+			}
+			assertNull(n.edges[3]);
+		}
+	}
+
+	/**
+	 * Need to shift elements after sorting to ensure cardinal directions.
+	 */
+	@Test
+	void sortEdgesCCW_shift() {
+		FastQueue<Node> corners = new FastQueue<>(Node.class,true);
+		for (int jump = 0; jump < 3; jump++) {
+			Node target = corners.grow();
+			target.x = 10;
+			target.y = 12;
+
+			for (int i = 0; i < 3; i++) {
+				double theta = i*Math.PI/2;
+				if( i > jump )
+					theta += Math.PI/2;
+				double c = Math.cos(theta);
+				double s = Math.sin(theta);
+
+				double r = 4;
+				Node n = new Node();
+				n.x = target.x + r*c;
+				n.y = target.y + r*s;
+				target.edges[i] = n;
+			}
+		}
+
+		ChessboardCornerClusterToGrid alg = new ChessboardCornerClusterToGrid();
+
+		alg.sortEdgesCCW(corners);
+
+		for( Node n : corners.toList() ) {
+			System.out.println();
+			Node m0 = n.edges[0];
+			double theta0 = Math.atan2(m0.y-n.y,m0.x-n.x);
+
+			for (int i = 0; i < 4; i++) {
+				Node m = n.edges[i];
+				if( m == null )
+					continue;
+
+				double thetaI = Math.atan2(m.y-n.y,m.x-n.x);
+
+				assertEquals(i*Math.PI/2.0,UtilAngle.distanceCCW(theta0,thetaI),0.001);
+			}
+		}
 	}
 
 	@Test
