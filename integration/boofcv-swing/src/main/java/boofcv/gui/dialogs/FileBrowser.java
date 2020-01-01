db0271794c255bb259db9a00f6cc13d7549e43a2
Peter Abeles <peter.abeles@gmail.com>
2020-01-01 10:13:47 -0800

    - FileBrowser: Up Directory goes up a directory when a file is selected - OpenImageSet: Increased preview size by reducing file list area

--- integration/boofcv-swing/src/main/java/boofcv/gui/dialogs/FileBrowser.java
+++ integration/boofcv-swing/src/main/java/boofcv/gui/dialogs/FileBrowser.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011-2019, Peter Abeles. All Rights Reserved.
+ * Copyright (c) 2011-2020, Peter Abeles. All Rights Reserved.
  *
  * This file is part of BoofCV (http://boofcv.org).
  *
@@ -156,6 +156,9 @@ public class FileBrowser extends JSpringPanel {
 		bUp.setToolTipText("Up Directory");
 		bUp.addActionListener(e->{
 			File f = new File(fileName.getText());
+			if( f.isFile() ) {
+				f = f.getParentFile();
+			}
 			setDirectory(f.getParentFile());
 		});
 
--- integration/boofcv-swing/src/main/java/boofcv/gui/dialogs/OpenImageSetDialog.java
+++ integration/boofcv-swing/src/main/java/boofcv/gui/dialogs/OpenImageSetDialog.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011-2019, Peter Abeles. All Rights Reserved.
+ * Copyright (c) 2011-2020, Peter Abeles. All Rights Reserved.
  *
  * This file is part of BoofCV (http://boofcv.org).
  *
@@ -89,7 +89,7 @@ public class OpenImageSetDialog extends JPanel {
         preview.setCentering(true);
 
         JSplitPane splitSelected = new JSplitPane(JSplitPane.VERTICAL_SPLIT,preview,selected);
-        splitSelected.setDividerLocation(100);
+        splitSelected.setDividerLocation(200);
         splitSelected.setResizeWeight(0.0);
         splitSelected.setContinuousLayout(true);
 
