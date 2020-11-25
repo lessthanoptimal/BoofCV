e367dd46807a2f8d921b39342899b6f03514afd3
Peter Abeles <peter.abeles@gmail.com>
2020-11-24 22:08:28 -0800

    Change in DDogleg - setMaxSize() -> reserve()

--- main/boofcv-recognition/src/main/java/boofcv/alg/fiducial/qrcode/ReidSolomonCodes.java
+++ main/boofcv-recognition/src/main/java/boofcv/alg/fiducial/qrcode/ReidSolomonCodes.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011-2017, Peter Abeles. All Rights Reserved.
+ * Copyright (c) 2020, Peter Abeles. All Rights Reserved.
  *
  * This file is part of BoofCV (http://boofcv.org).
  *
@@ -333,7 +333,8 @@ public class ReidSolomonCodes {
 	}
 
 	void initToOne( GrowQueue_I8 poly , int length ) {
-		poly.setMaxSize(length);
+		poly.reset();
+		poly.reserve(length);
 		poly.size = 1;
 		poly.data[0] = 1;
 	}
