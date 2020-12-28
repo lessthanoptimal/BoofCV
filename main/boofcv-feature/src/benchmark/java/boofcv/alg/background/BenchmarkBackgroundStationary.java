0a8911bcabe8e021fe7902f9b1c15654b3de5fef
Peter Abeles <peter.abeles@gmail.com>
2020-12-27 16:13:02 -0800

    - Updated to JMH 1.27 - Added a way to go from string abbreviation to ImageType - Two more benchmarks updated

--- main/boofcv-feature/src/benchmark/java/boofcv/alg/background/BenchmarkBackgroundStationary.java
+++ main/boofcv-feature/src/benchmark/java/boofcv/alg/background/BenchmarkBackgroundStationary.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011-2020, Peter Abeles. All Rights Reserved.
+ * Copyright (c) 2020, Peter Abeles. All Rights Reserved.
  *
  * This file is part of BoofCV (http://boofcv.org).
  *
@@ -25,125 +25,77 @@ import boofcv.factory.background.FactoryBackgroundModel;
 import boofcv.io.UtilIO;
 import boofcv.io.image.SimpleImageSequence;
 import boofcv.io.wrapper.DefaultMediaManager;
-import boofcv.struct.image.*;
+import boofcv.struct.image.GrayU8;
+import boofcv.struct.image.ImageBase;
+import boofcv.struct.image.ImageType;
+import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+import org.openjdk.jmh.runner.options.TimeValue;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Profiles the time to run each algorithm but ignores the time to load an image frame from the video
  *
  * @author Peter Abeles
  */
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.MILLISECONDS)
+@Warmup(iterations = 2)
+@Measurement(iterations = 5)
+@State(Scope.Benchmark)
+@Fork(value=1)
 @SuppressWarnings({"rawtypes", "unchecked"})
 public class BenchmarkBackgroundStationary {
 
-	File file;
+	File file = new File(UtilIO.pathExample("background/street_intersection.mp4"));
+
+	@Param({"SB_U8","PL_U8","SB_F32"})
+	String imageTypeName;
+
 	ImageType imageType;
 
-	public BenchmarkBackgroundStationary(File file, ImageType imageType) {
-		this.file = file;
-		this.imageType = imageType;
+	@Setup public void setup() {
+		imageType = ImageType.stringToType(imageTypeName,3);
 	}
 
-	public void benchmark() {
-		List<BackgroundBase> algs = new ArrayList<>();
-		algs.add(new Basic());
-		algs.add(new Gaussian());
-		algs.add(new GMM());
-
-		for( BackgroundBase b : algs ) {
-			b.process();
-			System.out.printf("%20s FPS = %.2f\n",b.getName(),b.getFPS());
-		}
+	@Benchmark public void greedy() {
+		var config = new ConfigBackgroundBasic(12);
+		process(FactoryBackgroundModel.stationaryBasic(config,imageType));
 	}
 
-	public class Basic extends BackgroundBase {
-
-		@Override
-		public BackgroundModelStationary create() {
-			ConfigBackgroundBasic config = new ConfigBackgroundBasic(12);
-
-			return FactoryBackgroundModel.stationaryBasic(config,imageType);
-		}
+	@Benchmark public void gaussian() {
+		var config =new ConfigBackgroundGaussian(12);
+		process(FactoryBackgroundModel.stationaryGaussian(config,imageType));
 	}
 
-	public class Gaussian extends BackgroundBase {
+	@Benchmark public void gmm() {
+		var config = new ConfigBackgroundGmm();
+		process(FactoryBackgroundModel.stationaryGmm(config,imageType));
+	}
 
-		@Override
-		public BackgroundModelStationary create() {
-			ConfigBackgroundGaussian config = new ConfigBackgroundGaussian(12);
+	private void process(BackgroundModelStationary alg) {
+		SimpleImageSequence sequence = DefaultMediaManager.INSTANCE.openVideo(file.getAbsolutePath(),imageType);
+		GrayU8 background = new GrayU8(sequence.getWidth(),sequence.getHeight());
 
-			return FactoryBackgroundModel.stationaryGaussian(config,imageType);
+		int total = 0;
+		while( sequence.hasNext() && total++ < 60 ) {
+			ImageBase image = sequence.next();
+			alg.updateBackground(image,background);
 		}
 	}
 
-	public class GMM extends BackgroundBase {
-		@Override
-		public BackgroundModelStationary create() {
-			ConfigBackgroundGmm config = new ConfigBackgroundGmm();
+	public static void main(String[] args) throws RunnerException {
+		Options opt = new OptionsBuilder()
+				.include(BenchmarkBackgroundStationary.class.getSimpleName())
+				.warmupTime(TimeValue.seconds(1))
+				.measurementTime(TimeValue.seconds(1))
+				.build();
 
-			return FactoryBackgroundModel.stationaryGmm(config,imageType);
-		}
-	}
-
-
-	public abstract class BackgroundBase {
-		SimpleImageSequence sequence;
-
-		long totalTime;
-		int frames;
-
-		GrayU8 background = new GrayU8(1,1);
-
-		public abstract BackgroundModelStationary create();
-
-		public void process() {
-			sequence = DefaultMediaManager.INSTANCE.openVideo(file.getAbsolutePath(),imageType);
-			background.reshape(sequence.getWidth(),sequence.getHeight());
-
-			totalTime = 0;
-			frames = 0;
-			BackgroundModelStationary model = create();
-
-			while( sequence.hasNext() ) {
-				ImageBase image = sequence.next();
-
-				long time0 = System.nanoTime();
-				model.updateBackground(image,background);
-				long time1 = System.nanoTime();
-
-				totalTime += (time1-time0);
-				frames++;
-			}
-		}
-
-		public double getFPS() {
-			return frames/(totalTime*1e-9);
-		}
-
-		public String getName() {
-			return getClass().getSimpleName();
-		}
-	}
-
-	public static void main(String[] args) {
-		File file = new File(UtilIO.pathExample("background/street_intersection.mp4"));
-
-		List<ImageType> imageTypes = new ArrayList<>();
-		imageTypes.add(ImageType.SB_U8);
-		imageTypes.add(ImageType.il(3,InterleavedU8.class));
-		imageTypes.add(ImageType.pl(3,GrayU8.class));
-		imageTypes.add(ImageType.SB_F32);
-		imageTypes.add(ImageType.il(3,InterleavedF32.class));
-		imageTypes.add(ImageType.pl(3,GrayF32.class));
-
-
-		for( ImageType type : imageTypes ) {
-			System.out.println("Image Type: "+type.toString());
-			BenchmarkBackgroundStationary b = new BenchmarkBackgroundStationary(file, type);
-			b.benchmark();
-		}
+		new Runner(opt).run();
 	}
 }
--- main/boofcv-feature/src/benchmark/java/boofcv/alg/descriptor/BenchmarkDescriptorDistance.java
+++ main/boofcv-feature/src/benchmark/java/boofcv/alg/descriptor/BenchmarkDescriptorDistance.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011-2017, Peter Abeles. All Rights Reserved.
+ * Copyright (c) 2020, Peter Abeles. All Rights Reserved.
  *
  * This file is part of BoofCV (http://boofcv.org).
  *
@@ -18,78 +18,76 @@
 
 package boofcv.alg.descriptor;
 
-import boofcv.misc.PerformerBase;
-import boofcv.misc.ProfileOperation;
 import boofcv.struct.feature.TupleDesc_B;
+import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+import org.openjdk.jmh.runner.options.TimeValue;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
-/**
- * @author Peter Abeles
- */
+@SuppressWarnings("ResultOfMethodCallIgnored") @BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.MILLISECONDS)
+@Warmup(iterations = 2)
+@Measurement(iterations = 5)
+@State(Scope.Benchmark)
+@Fork(value=1)
 public class BenchmarkDescriptorDistance {
 
-	static final long TEST_TIME = 1000;
-	static Random rand = new Random(234234);
-	static int NUM_FEATURES = 5000;
+	static int NUM_FEATURES = 10000;
 
-	static List<TupleDesc_B>  binaryA = new ArrayList<>();
-	static List<TupleDesc_B>  binaryB = new ArrayList<>();
+	List<TupleDesc_B>  binaryA = new ArrayList<>();
+	List<TupleDesc_B>  binaryB = new ArrayList<>();
+	HammingTable16 table = new HammingTable16();
 
-	static {
+	@Setup public void setup() {
+		Random rand = new Random(234234);
+		binaryA = new ArrayList<>();
+		binaryB = new ArrayList<>();
 		for (int i = 0; i < NUM_FEATURES; i++) {
-			binaryA.add(randomFeature());
-			binaryB.add(randomFeature());
+			binaryA.add(randomFeature(rand));
+			binaryB.add(randomFeature(rand));
 		}
 	}
 
-	public static class HammingTable extends PerformerBase {
-
-		HammingTable16 table = new HammingTable16();
-
-		@Override
-		public void process() {
-			for (int i = 0; i < binaryA.size(); i++) {
-				tableScore(binaryA.get(i),binaryB.get(i));
-			}
-		}
-
-		private int tableScore(TupleDesc_B a, TupleDesc_B b) {
-			int score = 0;
-
-			for( int i = 0; i < a.data.length; i++ ) {
-				int dataA = a.data[i];
-				int dataB = b.data[i];
-
-				score += table.lookup( (short)dataA , (short)dataB );
-				score += table.lookup( (short)(dataA >> 16) , (short)(dataB >> 16) );
-			}
-
-			return score;
+	@Benchmark public void hammingTable() {
+		for (int i = 0; i < binaryA.size(); i++) {
+			tableScore(binaryA.get(i),binaryB.get(i));
 		}
 	}
 
-	public static class HammingEquationOld extends PerformerBase {
-		@Override
-		public void process() {
-			for (int i = 0; i < binaryA.size(); i++) {
-				ExperimentalDescriptorDistance.hamming(binaryA.get(i),binaryB.get(i));
-			}
+	private int tableScore(TupleDesc_B a, TupleDesc_B b) {
+		int score = 0;
+
+		for( int i = 0; i < a.data.length; i++ ) {
+			int dataA = a.data[i];
+			int dataB = b.data[i];
+
+			score += table.lookup( (short)dataA , (short)dataB );
+			score += table.lookup( (short)(dataA >> 16) , (short)(dataB >> 16) );
+		}
+
+		return score;
+	}
+
+	@Benchmark public void equationOld() {
+		for (int i = 0; i < binaryA.size(); i++) {
+			ExperimentalDescriptorDistance.hamming(binaryA.get(i),binaryB.get(i));
 		}
 	}
 
-	public static class HammingEquation extends PerformerBase {
-		@Override
-		public void process() {
-			for (int i = 0; i < binaryA.size(); i++) {
-				DescriptorDistance.hamming(binaryA.get(i),binaryB.get(i));
-			}
+	@Benchmark public void equation() {
+		for (int i = 0; i < binaryA.size(); i++) {
+			DescriptorDistance.hamming(binaryA.get(i),binaryB.get(i));
 		}
 	}
 
-	private static TupleDesc_B randomFeature() {
+	private TupleDesc_B randomFeature( Random rand ) {
 		TupleDesc_B feat = new TupleDesc_B(512);
 		for (int j = 0; j < feat.data.length; j++) {
 			feat.data[j] = rand.nextInt();
@@ -97,13 +95,14 @@ public class BenchmarkDescriptorDistance {
 		return feat;
 	}
 
-	public static void main(String[] args) {
-		System.out.println("Larger numbers are better");
+	public static void main(String[] args) throws RunnerException {
+		Options opt = new OptionsBuilder()
+				.include(BenchmarkDescriptorDistance.class.getSimpleName())
+				.warmupTime(TimeValue.seconds(1))
+				.measurementTime(TimeValue.seconds(1))
+				.build();
 
-		ProfileOperation.printOpsPerSec(new HammingTable(),TEST_TIME);
-		ProfileOperation.printOpsPerSec(new HammingEquationOld(),TEST_TIME);
-		ProfileOperation.printOpsPerSec(new HammingEquation(),TEST_TIME);
+		new Runner(opt).run();
 	}
-
 
 }
