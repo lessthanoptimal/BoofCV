f29ad91798cb4c782377a6d8a488e2519aa7c0a0
Peter Abeles <peter.abeles@gmail.com>
2021-03-04 09:15:39 -0800

    CameraCalibration Application - Has a verbose mode and is set to false - Summary detection file - Statistics summary file - More informative error message

--- demonstrations/src/main/java/boofcv/demonstrations/calibration/CalibrateFisheyePlanarGuiApp.java
+++ demonstrations/src/main/java/boofcv/demonstrations/calibration/CalibrateFisheyePlanarGuiApp.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011-2020, Peter Abeles. All Rights Reserved.
+ * Copyright (c) 2021, Peter Abeles. All Rights Reserved.
  *
  * This file is part of BoofCV (http://boofcv.org).
  *
@@ -125,7 +125,7 @@ public class CalibrateFisheyePlanarGuiApp extends JPanel {
 		});
 
 		// print the output
-		calibrator.printStatistics();
+		calibrator.printStatistics(System.out);
 		System.out.println();
 		System.out.println("--- Intrinsic Parameters ---");
 		System.out.println();
--- demonstrations/src/main/java/boofcv/demonstrations/calibration/CalibratePinholePlanarGuiApp.java
+++ demonstrations/src/main/java/boofcv/demonstrations/calibration/CalibratePinholePlanarGuiApp.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011-2020, Peter Abeles. All Rights Reserved.
+ * Copyright (c) 2021, Peter Abeles. All Rights Reserved.
  *
  * This file is part of BoofCV (http://boofcv.org).
  *
@@ -140,7 +140,7 @@ public class CalibratePinholePlanarGuiApp extends JPanel
 		});
 
 		// print the output
-		calibrator.printStatistics();
+		calibrator.printStatistics(System.out);
 		System.out.println();
 		System.out.println("--- Intrinsic Parameters ---");
 		System.out.println();
