3f05a8964301de0714878a5b2bcdff2685c11229
Peter Abeles <peter.abeles@gmail.com>
2021-03-25 20:38:49 -0700

    GUI does something now

--- demonstrations/src/main/java/boofcv/demonstrations/recognition/DemoSceneRecognitionSimilarImagesApp.java
+++ demonstrations/src/main/java/boofcv/demonstrations/recognition/DemoSceneRecognitionSimilarImagesApp.java
@@ -25,14 +25,22 @@ import boofcv.factory.scene.FactorySceneRecognition;
 import boofcv.gui.BoofSwingUtil;
 import boofcv.gui.DemonstrationBase;
 import boofcv.gui.StandardAlgConfigPanel;
+import boofcv.gui.image.ImagePanel;
+import boofcv.gui.image.ScaleOptions;
 import boofcv.io.UtilIO;
+import boofcv.io.image.ConvertBufferedImage;
 import boofcv.io.image.UtilImageIO;
 import boofcv.misc.BoofMiscOps;
 import boofcv.struct.feature.TupleDesc;
 import boofcv.struct.image.*;
+import org.ddogleg.struct.DogArray;
+import org.ddogleg.struct.DogArray_I32;
 
 import javax.swing.*;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 import java.awt.*;
+import java.awt.geom.AffineTransform;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.util.ArrayList;
@@ -46,6 +54,8 @@ import static boofcv.io.UtilIO.systemToUnix;
 public class DemoSceneRecognitionSimilarImagesApp<Gray extends ImageGray<Gray>, TD extends TupleDesc<TD>>
 		extends DemonstrationBase {
 
+	public static final int PREVIEW_PIXELS = 400*300;
+
 	VisualizePanel gui = new VisualizePanel();
 	ControlPanel controlPanel = new ControlPanel();
 
@@ -53,7 +63,13 @@ public class DemoSceneRecognitionSimilarImagesApp<Gray extends ImageGray<Gray>, 
 	ImageType<Planar<Gray>> colorType;
 
 	// List of which images are similar to each other
-	List<List<String>> similarImages = new ArrayList<>();
+	final Object imageLock = new Object();
+	DogArray<DogArray_I32> imagesSimilar = new DogArray<>(DogArray_I32::new, DogArray_I32::reset);
+	List<String> imagePaths = new ArrayList<>();
+	List<BufferedImage> imagePreviews = new ArrayList<>();
+
+	// If true it's done doing all computations
+	boolean computingFinished;
 
 	protected DemoSceneRecognitionSimilarImagesApp( List<?> exampleInputs, Class<Gray> grayType ) {
 		super(true, false, exampleInputs);
@@ -116,76 +132,209 @@ public class DemoSceneRecognitionSimilarImagesApp<Gray extends ImageGray<Gray>, 
 			});
 		}
 
-		// TODO spawn thread so it doesn't block the UI
-		processImageFiles(foundFiles);
+		stopAllInputProcessing();
+		threadPool.execute(() -> {
+			setMenuBarEnabled(false);
+			computingFinished = false;
+			try {
+				processImageFiles(foundFiles);
+			} catch (RuntimeException e) {
+				e.printStackTrace();
+			}
+			computingFinished = true;
+			setMenuBarEnabled(true);
+		});
 	}
 
 	/**
 	 *
 	 */
 	void processImageFiles( List<String> foundFiles ) {
-		System.out.println("Processing images.size="+foundFiles.size());
+		System.out.println("Processing images.size=" + foundFiles.size());
+		// clean up
+		synchronized (imageLock) {
+			imagePaths.clear();
+			imagePreviews.clear();
+			imagesSimilar.reset();
+		}
+		SwingUtilities.invokeLater(() -> controlPanel.updateImagePaths());
 
 		var config = new ConfigSceneRecognitionSimilarImages();
 		config.features.detectFastHessian.extract.radius = 5;
 		config.features.detectFastHessian.maxFeaturesAll = 500;
 		config.limitMatchesConsider = 20;
 		config.minimumRatio = 0.3;
-		config.recognizeNister2006.learningMinimumPointsForChildren.setRelative(0.001,100);
+		config.recognizeNister2006.learningMinimumPointsForChildren.setRelative(0.001, 100);
 //		config.associate.greedy.scoreRatioThreshold = 0.9;
 		config.recognizeNister2006.minimumDepthFromRoot = 1;
 //		config.recognizeNister2006.featureSingleWordHops = 1;
 //		config.recognizeNister2006.learnNodeWeights = false;
 
-		SceneRecognitionSimilarImages<Gray,TD> alg =
+		SceneRecognitionSimilarImages<Gray, TD> alg =
 				FactorySceneRecognition.createSimilarImages(config, ImageType.single(grayType));
 
 		alg.setVerbose(System.out, null);
 
-		Gray gray = GeneralizedImageOps.createImage(grayType, 1, 1,0);
+		Gray gray = GeneralizedImageOps.createImage(grayType, 1, 1, 0);
 
 		System.out.print("Adding images: ");
 		long time0 = System.currentTimeMillis();
 		for (String path : foundFiles) {
-			UtilImageIO.loadImage(path, true, gray);
-			// TODO max resolution
+			BufferedImage buffered = UtilImageIO.loadImage(path);
 
+			// create the preview image
+			double scale = Math.sqrt(PREVIEW_PIXELS)/Math.sqrt(buffered.getWidth()*buffered.getHeight());
+			scale = Math.min(scale, 1.0);
+			BufferedImage preview = new BufferedImage(
+					(int)(buffered.getWidth()*scale), (int)(buffered.getHeight()*scale), BufferedImage.TYPE_INT_RGB);
+			preview.createGraphics().drawImage(buffered, AffineTransform.getScaleInstance(scale, scale), null);
+			synchronized (imageLock) {
+				imagePaths.add(path);
+				imagePreviews.add(preview);
+			}
+			SwingUtilities.invokeLater(() -> controlPanel.updateImagePaths());
+
+			// Convert to gray for image processing
+			ConvertBufferedImage.convertFrom(buffered, gray, true);
 			alg.addImage(path, gray);
 		}
 		long time1 = System.currentTimeMillis();
-		System.out.println((time1-time0)+" (ms)");
+		System.out.println((time1 - time0) + " (ms)");
 
 		System.out.print("Fixating: ");
 		alg.fixate();
 		long time2 = System.currentTimeMillis();
-		System.out.println((time2-time1)+" (ms)");
+		System.out.println((time2 - time1) + " (ms)");
 
 		System.out.print("Finding Similar: ");
-		similarImages.clear();
+		imagesSimilar.resize(0);
 		for (String path : foundFiles) {
-			List<String> similar = new ArrayList<>();
-			alg.findSimilar(path, similar);
-			similarImages.add(similar);
+			List<String> foundIDs = new ArrayList<>();
+			alg.findSimilar(path, foundIDs);
+
+			DogArray_I32 similar = imagesSimilar.grow();
+			for (String s : foundIDs) {
+				similar.add(foundFiles.indexOf(s));
+			}
 		}
 		long time3 = System.currentTimeMillis();
-		System.out.println((time3-time2)+" (ms)");
+		System.out.println((time3 - time2) + " (ms)");
 	}
 
-	class ControlPanel extends StandardAlgConfigPanel {
+	class ControlPanel extends StandardAlgConfigPanel implements ListSelectionListener {
 		protected JLabel processingTimeLabel = new JLabel();
 		protected JLabel imageSizeLabel = new JLabel();
 
-		public void setImageSize( final int width , final int height ) {
-			BoofSwingUtil.invokeNowOrLater(() -> imageSizeLabel.setText(width+" x "+height));
+		JList<String> listImages;
+
+		public ControlPanel() {
+			listImages = new JList<>();
+			listImages.setModel(new DefaultListModel<>());
+			listImages.setSelectionMode(ListSelectionModel.SINGLE_INTERVAL_SELECTION);
+			listImages.setLayoutOrientation(JList.VERTICAL);
+			listImages.setVisibleRowCount(-1);
+			listImages.addListSelectionListener(this);
+
+			addLabeled(processingTimeLabel, "Time");
+			addLabeled(imageSizeLabel, "Size");
+			addAlignCenter(new JScrollPane(listImages));
+			setPreferredSize(new Dimension(250, 500));
+		}
+
+		public void updateImagePaths() {
+			BoofSwingUtil.checkGuiThread();
+
+			// make a quick copy to avoid locking for long
+			List<String> copyPaths;
+			synchronized (imageLock) {
+				// Just add the file name to keep it short
+				copyPaths = new ArrayList<>(imagePaths.size());
+				for (String path : imagePaths) {
+					copyPaths.add(new File(path).getName());
+				}
+			}
+
+			this.listImages.removeListSelectionListener(this);
+			DefaultListModel<String> model = (DefaultListModel<String>)listImages.getModel();
+			model.clear();
+
+			model.addAll(copyPaths);
+
+			if (listImages.getSelectedIndex() >= copyPaths.size())
+				listImages.setSelectedIndex(0);
+			listImages.invalidate();
+			listImages.repaint();
+			this.listImages.addListSelectionListener(this);
+		}
+
+		public void setImageSize( final int width, final int height ) {
+			BoofSwingUtil.invokeNowOrLater(() -> imageSizeLabel.setText(width + " x " + height));
 		}
 
 		public void setProcessingTimeS( double seconds ) {
-			processingTimeLabel.setText(String.format("%7.1f",(seconds*1000)));
+			processingTimeLabel.setText(String.format("%7.1f", (seconds*1000)));
+		}
+
+		@Override public void valueChanged( ListSelectionEvent e ) {
+			int selected = listImages.getSelectedIndex();
+			if (selected != -1) {
+				gui.handleSelectedChanged();
+			}
 		}
 	}
 
 	class VisualizePanel extends JPanel {
+		ImagePanel mainImage = new ImagePanel();
 
+		JPanel gridPanel = new JPanel(new GridLayout(0, 4, 10, 10));
+
+		public VisualizePanel() {
+			setLayout(new BorderLayout());
+			mainImage.setScaling(ScaleOptions.DOWN);
+
+			JSplitPane splitPane = new JSplitPane(JSplitPane.VERTICAL_SPLIT,mainImage, gridPanel);
+			splitPane.setDividerLocation(150);
+			splitPane.setPreferredSize(new Dimension(200, 0));
+
+			add(splitPane, BorderLayout.CENTER);
+		}
+
+		public void handleSelectedChanged() {
+			BoofSwingUtil.checkGuiThread();
+
+			int selected = controlPanel.listImages.getSelectedIndex();
+			BufferedImage preview = null;
+			synchronized (imageLock) {
+				if (selected >= 0 && selected < imagePreviews.size()) {
+					preview = imagePreviews.get(selected);
+				}
+			}
+
+			// Nothing is selected so remove any images being displayed
+			if (preview == null) {
+				mainImage.setImageRepaint(null);
+				gridPanel.removeAll();
+				gridPanel.validate();
+				return;
+			}
+
+			mainImage.setImageRepaint(preview);
+
+			// Wait until it has finished before trying to visualize the results
+			if (!computingFinished) {
+				gridPanel.removeAll();
+				gridPanel.validate();
+				return;
+			}
+
+			DogArray_I32 similar = imagesSimilar.get(selected);
+			gridPanel.removeAll();
+			similar.forEach(imageIndex -> {
+				gridPanel.add(new ImagePanel(imagePreviews.get(imageIndex), ScaleOptions.DOWN));
+			});
+			gridPanel.validate();
+			gridPanel.repaint();
+		}
 	}
 
 	public static void main( String[] args ) {
