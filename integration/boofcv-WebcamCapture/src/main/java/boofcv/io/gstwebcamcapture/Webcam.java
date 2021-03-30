89faf9c95bfd6adb147db3c6e797355e0139947f
gatordevin <gatordevin@gmail.com>
2021-03-29 21:01:11 -0400

    FIxed Java spotless

--- integration/boofcv-WebcamCapture/src/main/java/boofcv/io/gstwebcamcapture/Webcam.java
+++ integration/boofcv-WebcamCapture/src/main/java/boofcv/io/gstwebcamcapture/Webcam.java
@@ -1,14 +1,23 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright (c) 2021, Peter Abeles. All Rights Reserved.
+ *
+ * This file is part of BoofCV (http://boofcv.org).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
+
 package boofcv.io.gstwebcamcapture;
 
-import boofcv.io.image.SimpleImageSequence;
-import boofcv.io.wrapper.WebcamInterface;
-import boofcv.struct.image.ImageBase;
-import boofcv.struct.image.ImageType;
 import java.awt.Dimension;
 import java.awt.image.BufferedImage;
 import java.util.ArrayList;
@@ -19,7 +28,6 @@ import org.freedesktop.gstreamer.Caps;
 import org.freedesktop.gstreamer.Pipeline;
 import org.freedesktop.gstreamer.Structure;
 import org.freedesktop.gstreamer.device.Device;
-import org.freedesktop.gstreamer.device.DeviceMonitor;
 
 /**
  *
