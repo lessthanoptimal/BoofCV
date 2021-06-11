e6be31dd93a0aa5095c6800c951597001134a5ee
Peter Abeles <peter.abeles@gmail.com>
2021-06-10 22:03:06 -0700

    Comments to explain why stuff isn't done

--- main/boofcv-geo/src/main/java/boofcv/alg/geo/structure/DecomposeAbsoluteDualQuadratic.java
+++ main/boofcv-geo/src/main/java/boofcv/alg/geo/structure/DecomposeAbsoluteDualQuadratic.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020, Peter Abeles. All Rights Reserved.
+ * Copyright (c) 2021, Peter Abeles. All Rights Reserved.
  *
  * This file is part of BoofCV (http://boofcv.org).
  *
@@ -48,24 +48,27 @@ public class DecomposeAbsoluteDualQuadratic {
 
 	/**
 	 * Decomposes the passed in absolute quadratic
+	 *
 	 * @param Q Absolute quadratic
 	 * @return true if successful or false if it failed
 	 */
 	public boolean decompose( DMatrix4x4 Q ) {
 
 		// scale Q so that Q(3,3) = 1 to provide a uniform scaling
-		CommonOps_DDF4.scale(1.0/Q.a33,Q);
+		CommonOps_DDF4.scale(1.0/Q.a33, Q);
 
-		// TODO consider using eigen decomposition like it was suggested
+		// TODO Consider using eigen decomposition like it was suggested.
+		//      Then the smallest eigenvalue can be forced to be zero. Having two classes that can be
+		//      compared against would probably be best.
 
 		// Directly extract from the definition of Q
 		// Q = [w -w*p;-p'*w p'*w*p]
 		// w = k*k'
-		k.a11 = Q.a11;k.a12 = Q.a12;k.a13 = Q.a13;
-		k.a21 = Q.a21;k.a22 = Q.a22;k.a23 = Q.a23;
-		k.a31 = Q.a31;k.a32 = Q.a32;k.a33 = Q.a33;
+		k.a11 = Q.a11; k.a12 = Q.a12; k.a13 = Q.a13;
+		k.a21 = Q.a21; k.a22 = Q.a22; k.a23 = Q.a23;
+		k.a31 = Q.a31; k.a32 = Q.a32; k.a33 = Q.a33;
 
-		if( !CommonOps_DDF3.invert(k,w_inv) )
+		if (!CommonOps_DDF3.invert(k, w_inv))
 			return false;
 		// force it to be positive definite. Solution will be of dubious value if this condition is triggered, but it
 		// seems to help much more often than it hurts
@@ -75,18 +78,18 @@ public class DecomposeAbsoluteDualQuadratic {
 		k.a22 = Math.abs(k.a22);
 		k.a33 = Math.abs(k.a33);
 
-		if( !CommonOps_DDF3.cholU(k) )
+		if (!CommonOps_DDF3.cholU(k))
 			return false;
-		if( !CommonOps_DDF3.invert(k,k) )
+		if (!CommonOps_DDF3.invert(k, k))
 			return false;
-		CommonOps_DDF3.divide(k,k.a33);
+		CommonOps_DDF3.divide(k, k.a33);
 
 		t.a1 = Q.a14; t.a2 = Q.a24; t.a3 = Q.a34;
 
 		CommonOps_DDF3.mult(w_inv, t, p);
-		CommonOps_DDF3.scale(-1,p);
+		CommonOps_DDF3.scale(-1, p);
 
-		CommonOps_DDF3.multTransB(k,k,w);
+		CommonOps_DDF3.multTransB(k, k, w);
 
 		return true;
 	}
@@ -97,19 +100,19 @@ public class DecomposeAbsoluteDualQuadratic {
 	 * @param Q Storage for the recomputed Q
 	 */
 	public void recomputeQ( DMatrix4x4 Q ) {
-		CommonOps_DDF3.multTransB(k,k,w);
+		CommonOps_DDF3.multTransB(k, k, w);
 
-		Q.a11 = w.a11;Q.a12 = w.a12;Q.a13 = w.a13;
-		Q.a21 = w.a21;Q.a22 = w.a22;Q.a23 = w.a23;
-		Q.a31 = w.a31;Q.a32 = w.a32;Q.a33 = w.a33;
+		Q.a11 = w.a11; Q.a12 = w.a12; Q.a13 = w.a13;
+		Q.a21 = w.a21; Q.a22 = w.a22; Q.a23 = w.a23;
+		Q.a31 = w.a31; Q.a32 = w.a32; Q.a33 = w.a33;
 
-		CommonOps_DDF3.mult(w,p,t);
-		CommonOps_DDF3.scale(-1,t);
+		CommonOps_DDF3.mult(w, p, t);
+		CommonOps_DDF3.scale(-1, t);
 
-		Q.a14 = t.a1;Q.a24 = t.a2;Q.a34 = t.a3;
-		Q.a41 = t.a1;Q.a42 = t.a2;Q.a43 = t.a3;
+		Q.a14 = t.a1; Q.a24 = t.a2; Q.a34 = t.a3;
+		Q.a41 = t.a1; Q.a42 = t.a2; Q.a43 = t.a3;
 
-		Q.a44 = -CommonOps_DDF3.dot(t,p);
+		Q.a44 = -CommonOps_DDF3.dot(t, p);
 	}
 
 	/**
@@ -118,21 +121,21 @@ public class DecomposeAbsoluteDualQuadratic {
 	 * H = [K 0; -p'*K 1]  see Pg 460
 	 */
 	public boolean computeRectifyingHomography( DMatrixRMaj H ) {
-		H.reshape(4,4);
+		H.reshape(4, 4);
 
 		// insert the results into H
 		// H = [K 0;-p'*K 1 ]
 		H.zero();
 		for (int i = 0; i < 3; i++) {
 			for (int j = i; j < 3; j++) {
-				H.set(i,j,k.get(i,j));
+				H.set(i, j, k.get(i, j));
 			}
 		}
 		// p and k have different scales, fix that
-		H.set(3,0, -(p.a1*k.a11 + p.a2*k.a21 + p.a3*k.a31));
-		H.set(3,1, -(p.a1*k.a12 + p.a2*k.a22 + p.a3*k.a32));
-		H.set(3,2, -(p.a1*k.a13 + p.a2*k.a23 + p.a3*k.a33));
-		H.set(3,3,1);
+		H.set(3, 0, -(p.a1*k.a11 + p.a2*k.a21 + p.a3*k.a31));
+		H.set(3, 1, -(p.a1*k.a12 + p.a2*k.a22 + p.a3*k.a32));
+		H.set(3, 2, -(p.a1*k.a13 + p.a2*k.a23 + p.a3*k.a33));
+		H.set(3, 3, 1);
 
 		return true;
 	}
@@ -147,6 +150,7 @@ public class DecomposeAbsoluteDualQuadratic {
 
 	/**
 	 * Coordinate of plane at infinity = pi_inf = (p,1)
+	 *
 	 * @return Coordinate of plane at infinity
 	 */
 	public DMatrix3 getP() {
