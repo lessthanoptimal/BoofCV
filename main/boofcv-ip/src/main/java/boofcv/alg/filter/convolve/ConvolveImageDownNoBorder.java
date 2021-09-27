e1badac3782dd843c321c862347b490538fc1a10
Peter Abeles <peter.abeles@gmail.com>
2021-09-27 16:44:28 -0700

    Fixed some runtime benchmarks for new regression test

--- main/boofcv-ip/src/main/java/boofcv/alg/filter/convolve/ConvolveImageDownNoBorder.java
+++ main/boofcv-ip/src/main/java/boofcv/alg/filter/convolve/ConvolveImageDownNoBorder.java
@@ -25,7 +25,6 @@ import boofcv.struct.convolve.Kernel2D_F32;
 import boofcv.struct.convolve.Kernel2D_S32;
 import boofcv.struct.image.*;
 
-
 /**
  * <p>
  * Specialized convolution where the center of the convolution skips over a constant number
@@ -33,156 +32,149 @@ import boofcv.struct.image.*;
  * resulting in it being at a lower resolution. A typical application for this is down sampling
  * inside an image pyramid.
  * </p>
- * 
+ *
  * @author Peter Abeles
  */
 public class ConvolveImageDownNoBorder {
 
-	public static void horizontal(Kernel1D_F32 kernel , GrayF32 input, GrayF32 output , int skip ) {
-		checkParametersH(input,output,skip);
+	public static void horizontal( Kernel1D_F32 kernel, GrayF32 input, GrayF32 output, int skip ) {
+		checkParametersH(input, output, skip);
 
-		if( !ConvolveDownNoBorderUnrolled_F32_F32.horizontal(kernel,input,output,skip)) {
-			ConvolveDownNoBorderStandard.horizontal(kernel,input,output,skip);
+		if (!ConvolveDownNoBorderUnrolled_F32_F32.horizontal(kernel, input, output, skip)) {
+			ConvolveDownNoBorderStandard.horizontal(kernel, input, output, skip);
 		}
 	}
 
-	public static void vertical(Kernel1D_F32 kernel , GrayF32 input, GrayF32 output , int skip ) {
-		checkParametersV(input,output,skip);
+	public static void vertical( Kernel1D_F32 kernel, GrayF32 input, GrayF32 output, int skip ) {
+		checkParametersV(input, output, skip);
 
-		if( !ConvolveDownNoBorderUnrolled_F32_F32.vertical(kernel,input,output,skip)) {
-			ConvolveDownNoBorderStandard.vertical(kernel,input,output,skip);
+		if (!ConvolveDownNoBorderUnrolled_F32_F32.vertical(kernel, input, output, skip)) {
+			ConvolveDownNoBorderStandard.vertical(kernel, input, output, skip);
 		}
 	}
 
-	public static void convolve(Kernel2D_F32 kernel , GrayF32 input, GrayF32 output , int skip ) {
-		checkParameters(input,output,skip);
+	public static void convolve( Kernel2D_F32 kernel, GrayF32 input, GrayF32 output, int skip ) {
+		checkParameters(input, output, skip);
 
-		if( !ConvolveDownNoBorderUnrolled_F32_F32.convolve(kernel,input,output,skip)) {
-			ConvolveDownNoBorderStandard.convolve(kernel,input,output,skip);
+		if (!ConvolveDownNoBorderUnrolled_F32_F32.convolve(kernel, input, output, skip)) {
+			ConvolveDownNoBorderStandard.convolve(kernel, input, output, skip);
 		}
 	}
 
-	public static void horizontal(Kernel1D_S32 kernel , GrayU8 input, GrayI16 output , int skip ) {
-		checkParametersH(input,output,skip);
+	public static void horizontal( Kernel1D_S32 kernel, GrayU8 input, GrayI16 output, int skip ) {
+		checkParametersH(input, output, skip);
 
-		if( !ConvolveDownNoBorderUnrolled_U8_I16.horizontal(kernel,input,output,skip)) {
-			ConvolveDownNoBorderStandard.horizontal(kernel,input,output,skip);
+		if (!ConvolveDownNoBorderUnrolled_U8_I16.horizontal(kernel, input, output, skip)) {
+			ConvolveDownNoBorderStandard.horizontal(kernel, input, output, skip);
 		}
 	}
 
-	public static void vertical(Kernel1D_S32 kernel , GrayU8 input, GrayI16 output , int skip ) {
-		checkParametersV(input,output,skip);
+	public static void vertical( Kernel1D_S32 kernel, GrayU8 input, GrayI16 output, int skip ) {
+		checkParametersV(input, output, skip);
 
-		if( !ConvolveDownNoBorderUnrolled_U8_I16.vertical(kernel,input,output,skip)) {
-			ConvolveDownNoBorderStandard.vertical(kernel,input,output,skip);
+		if (!ConvolveDownNoBorderUnrolled_U8_I16.vertical(kernel, input, output, skip)) {
+			ConvolveDownNoBorderStandard.vertical(kernel, input, output, skip);
 		}
 	}
 
-	public static void convolve(Kernel2D_S32 kernel , GrayU8 input, GrayI16 output , int skip ) {
-		checkParameters(input,output,skip);
+	public static void convolve( Kernel2D_S32 kernel, GrayU8 input, GrayI16 output, int skip ) {
+		checkParameters(input, output, skip);
 
-		if( !ConvolveDownNoBorderUnrolled_U8_I16.convolve(kernel,input,output,skip)) {
-			ConvolveDownNoBorderStandard.convolve(kernel,input,output,skip);
+		if (!ConvolveDownNoBorderUnrolled_U8_I16.convolve(kernel, input, output, skip)) {
+			ConvolveDownNoBorderStandard.convolve(kernel, input, output, skip);
 		}
 	}
 
-	public static void horizontal(Kernel1D_S32 kernel , GrayS16 input, GrayI16 output , int skip ) {
-		checkParametersH(input,output,skip);
+	public static void horizontal( Kernel1D_S32 kernel, GrayS16 input, GrayI16 output, int skip ) {
+		checkParametersH(input, output, skip);
 
-		if( !ConvolveDownNoBorderUnrolled_S16_I16.horizontal(kernel,input,output,skip)) {
-			ConvolveDownNoBorderStandard.horizontal(kernel,input,output,skip);
+		if (!ConvolveDownNoBorderUnrolled_S16_I16.horizontal(kernel, input, output, skip)) {
+			ConvolveDownNoBorderStandard.horizontal(kernel, input, output, skip);
 		}
 	}
 
-	public static void vertical(Kernel1D_S32 kernel , GrayS16 input, GrayI16 output , int skip ) {
-		checkParametersV(input,output,skip);
+	public static void vertical( Kernel1D_S32 kernel, GrayS16 input, GrayI16 output, int skip ) {
+		checkParametersV(input, output, skip);
 
-		if( !ConvolveDownNoBorderUnrolled_S16_I16.vertical(kernel,input,output,skip)) {
-			ConvolveDownNoBorderStandard.vertical(kernel,input,output,skip);
+		if (!ConvolveDownNoBorderUnrolled_S16_I16.vertical(kernel, input, output, skip)) {
+			ConvolveDownNoBorderStandard.vertical(kernel, input, output, skip);
 		}
 	}
 
-	public static void convolve(Kernel2D_S32 kernel , GrayS16 input, GrayI16 output , int skip ) {
-		checkParameters(input,output,skip);
+	public static void convolve( Kernel2D_S32 kernel, GrayS16 input, GrayI16 output, int skip ) {
+		checkParameters(input, output, skip);
 
-		if( !ConvolveDownNoBorderUnrolled_S16_I16.convolve(kernel,input,output,skip)) {
-			ConvolveDownNoBorderStandard.convolve(kernel,input,output,skip);
+		if (!ConvolveDownNoBorderUnrolled_S16_I16.convolve(kernel, input, output, skip)) {
+			ConvolveDownNoBorderStandard.convolve(kernel, input, output, skip);
 		}
 	}
 
-	public static void horizontal(Kernel1D_S32 kernel , GrayU8 input, GrayI8 output , int skip , int divisor ) {
-		checkParametersH(input,output,skip);
+	public static void horizontal( Kernel1D_S32 kernel, GrayU8 input, GrayI8 output, int skip, int divisor ) {
+		checkParametersH(input, output, skip);
 
-		if( !ConvolveDownNoBorderUnrolled_U8_I8_Div.horizontal(kernel,input,output,skip,divisor)) {
-			ConvolveDownNoBorderStandard.horizontal(kernel,input,output,skip,divisor);
+		if (!ConvolveDownNoBorderUnrolled_U8_I8_Div.horizontal(kernel, input, output, skip, divisor)) {
+			ConvolveDownNoBorderStandard.horizontal(kernel, input, output, skip, divisor);
 		}
 	}
 
-	public static void vertical(Kernel1D_S32 kernel , GrayU8 input, GrayI8 output , int skip , int divisor ) {
-		checkParametersV(input,output,skip);
+	public static void vertical( Kernel1D_S32 kernel, GrayU8 input, GrayI8 output, int skip, int divisor ) {
+		checkParametersV(input, output, skip);
 
-		if( !ConvolveDownNoBorderUnrolled_U8_I8_Div.vertical(kernel,input,output,skip,divisor)) {
-			ConvolveDownNoBorderStandard.vertical(kernel,input,output,skip,divisor);
+		if (!ConvolveDownNoBorderUnrolled_U8_I8_Div.vertical(kernel, input, output, skip, divisor)) {
+			ConvolveDownNoBorderStandard.vertical(kernel, input, output, skip, divisor);
 		}
 	}
 
-	public static void convolve(Kernel2D_S32 kernel , GrayU8 input, GrayI8 output , int skip , int divisor ) {
-		checkParameters(input,output,skip);
+	public static void convolve( Kernel2D_S32 kernel, GrayU8 input, GrayI8 output, int skip, int divisor ) {
+		checkParameters(input, output, skip);
 
-		if( !ConvolveDownNoBorderUnrolled_U8_I8_Div.convolve(kernel,input,output,skip,divisor)) {
-			ConvolveDownNoBorderStandard.convolve(kernel,input,output,skip,divisor);
+		if (!ConvolveDownNoBorderUnrolled_U8_I8_Div.convolve(kernel, input, output, skip, divisor)) {
+			ConvolveDownNoBorderStandard.convolve(kernel, input, output, skip, divisor);
 		}
 	}
 
-	public static void horizontal(Kernel1D_S32 kernel , GrayS16 input, GrayI16 output , int skip , int divisor ) {
-		checkParametersH(input,output,skip);
+	public static void horizontal( Kernel1D_S32 kernel, GrayS16 input, GrayI16 output, int skip, int divisor ) {
+		checkParametersH(input, output, skip);
 
-		if( !ConvolveDownNoBorderUnrolled_S16_I16_Div.horizontal(kernel,input,output,skip,divisor)) {
-			ConvolveDownNoBorderStandard.horizontal(kernel,input,output,skip,divisor);
+		if (!ConvolveDownNoBorderUnrolled_S16_I16_Div.horizontal(kernel, input, output, skip, divisor)) {
+			ConvolveDownNoBorderStandard.horizontal(kernel, input, output, skip, divisor);
 		}
 	}
 
-	public static void vertical(Kernel1D_S32 kernel , GrayS16 input, GrayI16 output , int skip , int divisor ) {
-		checkParametersV(input,output,skip);
+	public static void vertical( Kernel1D_S32 kernel, GrayS16 input, GrayI16 output, int skip, int divisor ) {
+		checkParametersV(input, output, skip);
 
-		if( !ConvolveDownNoBorderUnrolled_S16_I16_Div.vertical(kernel,input,output,skip,divisor)) {
-			ConvolveDownNoBorderStandard.vertical(kernel,input,output,skip,divisor);
+		if (!ConvolveDownNoBorderUnrolled_S16_I16_Div.vertical(kernel, input, output, skip, divisor)) {
+			ConvolveDownNoBorderStandard.vertical(kernel, input, output, skip, divisor);
 		}
 	}
 
-	public static void convolve(Kernel2D_S32 kernel , GrayS16 input, GrayI16 output , int skip , int divisor ) {
-		checkParameters(input,output,skip);
+	public static void convolve( Kernel2D_S32 kernel, GrayS16 input, GrayI16 output, int skip, int divisor ) {
+		checkParameters(input, output, skip);
 
-		if( !ConvolveDownNoBorderUnrolled_S16_I16_Div.convolve(kernel,input,output,skip,divisor)) {
-			ConvolveDownNoBorderStandard.convolve(kernel,input,output,skip,divisor);
+		if (!ConvolveDownNoBorderUnrolled_S16_I16_Div.convolve(kernel, input, output, skip, divisor)) {
+			ConvolveDownNoBorderStandard.convolve(kernel, input, output, skip, divisor);
 		}
 	}
-	public static void checkParametersH(ImageGray input , ImageGray output , int skip ) {
-		if( skip <= 0  )
+
+	public static void checkParametersH( ImageGray input, ImageGray output, int skip ) {
+		if (skip <= 0)
 			throw new IllegalArgumentException("Skip must be >= 1");
-		if( output.width < input.width/skip )
-			throw new IllegalArgumentException("Output width is too small: output.width = "+output.width+" input.width/skip "+ input.width/skip);
-		if( output.height < input.height )
-			throw new IllegalArgumentException("Output height is too small: output.height = "+output.height+" input.height "+input.height);
+		if (output.width < input.width/skip || output.height < input.height)
+			output.reshape(input.width/skip, input.height);
 	}
 
-	public static void checkParametersV(ImageGray input , ImageGray output , int skip ) {
-		if( skip <= 0  )
+	public static void checkParametersV( ImageGray input, ImageGray output, int skip ) {
+		if (skip <= 0)
 			throw new IllegalArgumentException("Skip must be >= 1");
-		if( output.width < input.width )
-			throw new IllegalArgumentException("Output width is too small: output.width = "+output.width+" input.width "+ input.width);
-		if( output.height < input.height/skip )
-			throw new IllegalArgumentException("Output height is too small: output.height = "+output.height+" input.height/skip "+input.height/skip);;
+		if (output.width < input.width || output.height < input.height/skip)
+			output.reshape(input.width, input.height/skip);
 	}
 
-	public static void checkParameters(ImageGray input , ImageGray output , int skip ) {
-		if( skip <= 0  )
+	public static void checkParameters( ImageGray input, ImageGray output, int skip ) {
+		if (skip <= 0)
 			throw new IllegalArgumentException("Skip must be >= 1");
-		if( output.width < input.width/skip )
-			throw new IllegalArgumentException("Output width is too small: output.width = "+output.width+" input.width/skip "+ input.width/skip);
-		if( output.height < input.height/skip )
-			throw new IllegalArgumentException("Output height is too small: output.height = "+output.height+" input.height/skip "+input.height/skip);
-
+		if (output.width < input.width/skip || output.height < input.height/skip)
+			output.reshape(input.width/skip, input.height/skip);
 	}
-
 }
