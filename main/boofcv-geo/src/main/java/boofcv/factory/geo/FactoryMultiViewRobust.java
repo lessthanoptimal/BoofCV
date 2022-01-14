82b943f023078b51ef7e33371bd37f29b29bfc6f
Peter Abeles <peter.abeles@gmail.com>
2022-01-13 18:40:45 -0800

    Updating Nullable

--- main/boofcv-geo/src/main/java/boofcv/factory/geo/FactoryMultiViewRobust.java
+++ main/boofcv-geo/src/main/java/boofcv/factory/geo/FactoryMultiViewRobust.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021, Peter Abeles. All Rights Reserved.
+ * Copyright (c) 2022, Peter Abeles. All Rights Reserved.
  *
  * This file is part of BoofCV (http://boofcv.org).
  *
@@ -60,7 +60,6 @@ import org.jetbrains.annotations.Nullable;
  *
  * @author Peter Abeles
  */
-@SuppressWarnings("NullAway") // for NullAway bug
 public class FactoryMultiViewRobust {
 
 	/**
