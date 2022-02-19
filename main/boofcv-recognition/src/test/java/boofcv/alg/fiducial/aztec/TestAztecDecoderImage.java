5426a6074fc21a704e49e741c82868460fb37a76
Peter Abeles <peter.abeles@gmail.com>
2022-02-19 15:36:13 -0800

    feature/aztec_transpose - Now handles transposed markers

--- main/boofcv-recognition/src/test/java/boofcv/alg/fiducial/aztec/TestAztecDecoderImage.java
+++ main/boofcv-recognition/src/test/java/boofcv/alg/fiducial/aztec/TestAztecDecoderImage.java
@@ -19,6 +19,7 @@
 package boofcv.alg.fiducial.aztec;
 
 import boofcv.alg.fiducial.qrcode.PackedBits8;
+import boofcv.alg.misc.ImageMiscOps;
 import boofcv.struct.image.GrayU8;
 import boofcv.testing.BoofStandardJUnit;
 import georegression.geometry.UtilPolygons2D_F64;
@@ -33,23 +34,29 @@ class TestAztecDecoderImage extends BoofStandardJUnit {
 	 */
 	@Test void process() {
 		var alg = new AztecDecoderImage<>(GrayU8.class);
-		for (var structure : AztecCode.Structure.values()) {
-			AztecCode truth = new AztecEncoder().setStructure(structure).addUpper("TEST").fixate();
-			GrayU8 image = AztecGenerator.renderImage(10, 0, truth);
+		for (boolean transposed : new boolean[]{false, true}) {
+			for (var structure : AztecCode.Structure.values()) {
+				AztecCode truth = new AztecEncoder().setStructure(structure).addUpper("TEST").fixate();
+				GrayU8 image = AztecGenerator.renderImage(10, 0, truth);
 
-			AztecCode found = new AztecCode();
+				if (transposed) {
+					ImageMiscOps.transpose(image.clone(), image);
+				}
 
-			for (int rotation = 0; rotation < 4; rotation++) {
-				// Process the image
-				assertTrue(alg.process(truth.locator, image, found));
+				AztecCode found = new AztecCode();
 
-				// See if it was decoded correctly
-				assertEquals(truth.message, found.message);
-				assertEquals(0, found.totalBitErrors);
-				assertFalse(found.transposed);
+				for (int rotation = 0; rotation < 4; rotation++) {
+					// Process the image
+					assertTrue(alg.process(truth.locator, image, found));
 
-				// Rotate the locator pattern so that initially it will be wrong
-				truth.locator.layers.forEach(l -> UtilPolygons2D_F64.shiftUp(l.square));
+					// See if it was decoded correctly
+					assertEquals(truth.message, found.message);
+					assertEquals(0, found.totalBitErrors);
+					assertEquals(transposed, found.transposed);
+
+					// Rotate the locator pattern so that initially it will be wrong
+					truth.locator.layers.forEach(l -> UtilPolygons2D_F64.shiftUp(l.square));
+				}
 			}
 		}
 	}
@@ -100,7 +107,7 @@ class TestAztecDecoderImage extends BoofStandardJUnit {
 			alg.readModeBitsFromImage(truth.locator);
 
 			// Fixed structure should have no error
-			assertEquals(0, alg.fixedFeatureReadErrors(0, AztecDecoderImage.getModeBitType(structure)));
+			assertEquals(0, alg.fixedFeatureReadErrors(false, 0, AztecDecoderImage.getModeBitType(structure)));
 
 			// See if the data is as expected by encoding it and comparing
 			alg.extractModeDataBits(0, structure);
@@ -112,16 +119,50 @@ class TestAztecDecoderImage extends BoofStandardJUnit {
 		}
 	}
 
-	@Test void selectOrientation() {
+	@Test void selectOrientationAndTranspose() {
 		var alg = new AztecDecoderImage<>(GrayU8.class);
 
-		for (int side = 0; side < 4; side++) {
-			// Fill in mode bits with a known pattern with the specified side first
-			modeImageBitsCompact((4 - side)%4, alg.imageBits);
+		for (boolean transposed : new boolean[]{false, true}) {
+			for (int side = 0; side < 4; side++) {
+				// Fill in mode bits with a known pattern with the specified side first
+				modeImageBitsCompact((4 - side)%4, alg.imageBits);
 
-			// see if it selects the correct side/orientation
-			assertEquals(side, alg.selectOrientation(AztecCode.Structure.COMPACT));
+				// transpose the input
+				if (transposed) {
+					AztecDecoderImage.transposeModeBitArray(alg.imageBits, alg.bits);
+				}
+
+				// see if it selects the correct side/orientation + transpose
+				int expected = side + (transposed ? 4 : 0);
+				assertEquals(expected, alg.selectOrientationAndTranspose(AztecCode.Structure.COMPACT));
+			}
 		}
+	}
+
+	@Test void transposeModeBitIndex() {
+		assertEquals(0, AztecDecoderImage.transposeModeBitIndex(0, 40));
+		assertEquals(39, AztecDecoderImage.transposeModeBitIndex(1, 40));
+		assertEquals(1, AztecDecoderImage.transposeModeBitIndex(39, 40));
+		assertEquals(21, AztecDecoderImage.transposeModeBitIndex(19, 40));
+		assertEquals(20, AztecDecoderImage.transposeModeBitIndex(20, 40));
+		assertEquals(19, AztecDecoderImage.transposeModeBitIndex(21, 40));
+		assertEquals(31, AztecDecoderImage.transposeModeBitIndex(9, 40));
+		assertEquals(30, AztecDecoderImage.transposeModeBitIndex(10, 40));
+		assertEquals(29, AztecDecoderImage.transposeModeBitIndex(11, 40));
+	}
+
+	/**
+	 * Transpose mode bits that are around a 2x2 inner grid (4x4 outer ring)
+	 */
+	@Test void transposeModeBitArray() {
+		var bits = new PackedBits8();
+		bits.append("101000111010");
+
+		var expected = new PackedBits8();
+		expected.append("101011100010");
+
+		AztecDecoderImage.transposeModeBitArray(bits, new PackedBits8());
+		assertTrue(expected.isIdentical(bits));
 	}
 
 	@Test void extractModeDataBits() {
@@ -153,15 +194,15 @@ class TestAztecDecoderImage extends BoofStandardJUnit {
 			modeImageBitsCompact(antiSide, alg.imageBits);
 
 			// when given the correct offset there should be no error
-			assertEquals(0, alg.fixedFeatureReadErrors(startSide*10, bitTypes));
+			assertEquals(0, alg.fixedFeatureReadErrors(false, startSide*10, bitTypes));
 
 			// if the offset is wrong then there should be an error
 			int incorrectSide = (startSide + 1)%4;
-			assertNotEquals(0, alg.fixedFeatureReadErrors(incorrectSide*10, bitTypes));
+			assertNotEquals(0, alg.fixedFeatureReadErrors(false, incorrectSide*10, bitTypes));
 
 			// Introduce a single bit error
 			alg.imageBits.set(11, alg.imageBits.get(11) ^ 1);
-			assertEquals(1, alg.fixedFeatureReadErrors(startSide*10, bitTypes));
+			assertEquals(1, alg.fixedFeatureReadErrors(false, startSide*10, bitTypes));
 		}
 	}
 
