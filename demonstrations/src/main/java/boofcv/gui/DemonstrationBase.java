8d518c344f6021a6a1611a46a4e871a0f1b01f10
Peter Abeles <peter.abeles@gmail.com>
2022-02-20 09:28:27 -0800

    DemonstrationBase - Fixed case where there's a space in the path and next fails

--- demonstrations/src/main/java/boofcv/gui/DemonstrationBase.java
+++ demonstrations/src/main/java/boofcv/gui/DemonstrationBase.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021, Peter Abeles. All Rights Reserved.
+ * Copyright (c) 2022, Peter Abeles. All Rights Reserved.
  *
  * This file is part of BoofCV (http://boofcv.org).
  *
@@ -493,7 +493,7 @@ public abstract class DemonstrationBase extends JPanel {
 			e.printStackTrace();
 			return;
 		}
-		File current = new File(UtilIO.ensureUrlNotNull(path).getFile());
+		File current = new File(UtilIO.ensureUrlNotNull(path).getFile().replace("%20"," "));
 		File parent = current.getParentFile();
 		if (parent == null)
 			return;
