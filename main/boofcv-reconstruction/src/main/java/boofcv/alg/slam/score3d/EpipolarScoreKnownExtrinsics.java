e1be76db17c84a6e6cf550dd728162fd5770a898
Peter Abeles <peter.abeles@gmail.com>
2022-04-20 20:24:17 -0700

    feature/slam - Added unit test for EpipolarScoreKnownExtrinsics - Changed behavior so that smaller residual error is preferred

--- main/boofcv-reconstruction/src/main/java/boofcv/alg/slam/score3d/EpipolarScoreKnownExtrinsics.java
+++ main/boofcv-reconstruction/src/main/java/boofcv/alg/slam/score3d/EpipolarScoreKnownExtrinsics.java
@@ -30,6 +30,8 @@ import georegression.struct.point.Point2D_F64;
 import georegression.struct.point.Point3D_F64;
 import georegression.struct.point.Point4D_F64;
 import georegression.struct.se.Se3_F64;
+import georegression.transform.se.SePointOps_F64;
+import lombok.Getter;
 import org.ddogleg.struct.DogArray_I32;
 import org.jetbrains.annotations.Nullable;
 
@@ -48,8 +50,12 @@ import java.util.Set;
 public class EpipolarScoreKnownExtrinsics implements EpipolarCalibratedScore3D {
 
 	/** Specifies max reproejction error for inlier. If relative, then width + height */
-	public ConfigLength maxReprojectionError = ConfigLength.relative(0.006, 2);
+	@Getter public ConfigLength maxReprojectionError = ConfigLength.relative(0.006, 2);
 
+	/** Larger values reduces influence of residual error */
+	@Getter public double dampeneddResidualError = 50;
+
+	// Triangulates location of points
 	Triangulate2PointingMetricH triangulator = FactoryMultiView.triangulate2PointingMetricH(null);
 
 	// Predicted and observed pixel coordinates
@@ -64,6 +70,9 @@ public class EpipolarScoreKnownExtrinsics implements EpipolarCalibratedScore3D {
 
 	// error tolerance for both images
 	double errorTolA, errorTolB;
+
+	double residualError;
+	double residualErrorNoTranslation;
 
 	// Computed 3D score
 	double score3D;
@@ -90,29 +99,32 @@ public class EpipolarScoreKnownExtrinsics implements EpipolarCalibratedScore3D {
 		errorTolA *= errorTolA;
 		errorTolB *= errorTolB;
 
-		findInliers(pointToPixelA, pointToPixelB, obsA, pairs, a_to_b, inliersIdx);
+		findInliers(pointToPixelA, pointToPixelB, obsA, obsB, pairs, a_to_b, inliersIdx);
 
-		int countNoTranslation = findNoTranslationInliers(pointToPixelA, pointToPixelB, obsA, pairs, inliersIdx);
+		int countNoTranslation = findNoTranslationInliers(pointToPixelA, pointToPixelB, obsA, obsB, pairs, inliersIdx);
 
 		if (verbose != null) verbose.printf("inliers=%d no_translation=%d\n", inliersIdx.size, countNoTranslation);
 
 		// Want a higher score when there are more inliers, want a lower score when they are still inliers
 		// after translation has been removed. Adjust so that a value close to zero is bad
-		score3D = inliersIdx.size/(2.0 + countNoTranslation) - 1.0;
-		is3D = inliersIdx.size > countNoTranslation*2;
+		score3D = inliersIdx.size/(2.0 + countNoTranslation);
+		// Prefer smaller residual error and larger error with no translation
+		score3D *= (dampeneddResidualError + residualErrorNoTranslation)/(dampeneddResidualError + residualError);
+		is3D = inliersIdx.size > (countNoTranslation + 1)*2;
 	}
 
 	/**
 	 * Finds inliers by triangulating the points then seeing if the reprojection error in each view is too large
 	 */
 	private void findInliers( Point3Transform2_F64 pointToPixelA, Point3Transform2_F64 pointToPixelB,
-							  List<Point3D_F64> obsA, List<AssociatedIndex> pairs,
+							  List<Point3D_F64> obsA, List<Point3D_F64> obsB, List<AssociatedIndex> pairs,
 							  Se3_F64 a_to_b, DogArray_I32 inliersIdx ) {
 
+		double totalError = 0;
 		for (int i = 0; i < pairs.size(); i++) {
 			AssociatedIndex associated = pairs.get(i);
 			Point3D_F64 oa = obsA.get(associated.src);
-			Point3D_F64 ob = obsA.get(associated.dst);
+			Point3D_F64 ob = obsB.get(associated.dst);
 
 			if (!triangulator.triangulate(oa, ob, a_to_b, X)) {
 				if (verbose != null) verbose.println("Failed to triangulate? Bad points? " + oa + " " + ob);
@@ -122,31 +134,38 @@ public class EpipolarScoreKnownExtrinsics implements EpipolarCalibratedScore3D {
 			// Reproject the triangulated point and the original observation
 			pointToPixelA.compute(X.x, X.y, X.z, predicted);
 			pointToPixelA.compute(oa.x, oa.y, oa.z, observed);
+			double errorA = observed.distance2(predicted);
 
 			// See if the error is too large and this observation is an outlier
-			if (observed.distance2(predicted) > errorTolA)
+			if (errorA > errorTolA)
 				continue;
 
+			SePointOps_F64.transform(a_to_b, X, X);
 			pointToPixelB.compute(X.x, X.y, X.z, predicted);
 			pointToPixelB.compute(ob.x, ob.y, ob.z, observed);
-			if (observed.distance2(predicted) > errorTolB)
+			double errorB = observed.distance2(predicted);
+			if (errorB > errorTolB)
 				continue;
 
+			totalError += errorA + errorB;
 			inliersIdx.add(i);
 		}
+
+		residualError = Math.sqrt(totalError/(1 + inliersIdx.size));
 	}
 
 	/**
 	 * Finds the number of inliers if the translation is set to zero
 	 */
 	private int findNoTranslationInliers( Point3Transform2_F64 pointToPixelA, Point3Transform2_F64 pointToPixelB,
-										  List<Point3D_F64> obsA, List<AssociatedIndex> pairs, DogArray_I32 inliersIdx ) {
+										  List<Point3D_F64> obsA, List<Point3D_F64> obsB, List<AssociatedIndex> pairs, DogArray_I32 inliersIdx ) {
 		int remainingInliers = 0;
+		double totalError = 0;
 		for (int idx = 0; idx < inliersIdx.size; idx++) {
 			int i = inliersIdx.get(idx);
 			AssociatedIndex associated = pairs.get(i);
 			Point3D_F64 oa = obsA.get(associated.src);
-			Point3D_F64 ob = obsA.get(associated.dst);
+			Point3D_F64 ob = obsB.get(associated.dst);
 
 			if (!triangulator.triangulate(oa, ob, motionNoTranslation, X)) {
 				if (verbose != null) verbose.println("Failed to triangulate with no translation");
@@ -155,16 +174,25 @@ public class EpipolarScoreKnownExtrinsics implements EpipolarCalibratedScore3D {
 
 			pointToPixelA.compute(X.x, X.y, X.z, predicted);
 			pointToPixelA.compute(oa.x, oa.y, oa.z, observed);
-			if (observed.distance2(predicted) > errorTolA)
-				continue;
+			double errorA = observed.distance2(predicted);
 
+
+			SePointOps_F64.transform(motionNoTranslation, X, X);
 			pointToPixelB.compute(X.x, X.y, X.z, predicted);
 			pointToPixelB.compute(ob.x, ob.y, ob.z, observed);
-			if (observed.distance2(predicted) > errorTolB)
+			double errorB = observed.distance2(predicted);
+
+			totalError += errorA + errorB;
+
+			if (errorA > errorTolA)
+				continue;
+			if (errorB > errorTolB)
 				continue;
 
 			remainingInliers++;
 		}
+		residualErrorNoTranslation = Math.sqrt(totalError/(1 + inliersIdx.size));
+
 		return remainingInliers;
 	}
 
--- main/boofcv-reconstruction/src/test/java/boofcv/alg/slam/score3d/TestEpipolarScoreKnownExtrinsics.java
+++ main/boofcv-reconstruction/src/test/java/boofcv/alg/slam/score3d/TestEpipolarScoreKnownExtrinsics.java
@@ -18,12 +18,84 @@
 
 package boofcv.alg.slam.score3d;
 
+import boofcv.alg.distort.SphereToNarrowPixel_F64;
+import boofcv.factory.distort.LensDistortionFactory;
+import boofcv.struct.calib.CameraPinholeBrown;
+import boofcv.struct.distort.Point3Transform2_F64;
+import boofcv.struct.feature.AssociatedIndex;
+import boofcv.testing.BoofStandardJUnit;
+import georegression.geometry.UtilPoint3D_F64;
+import georegression.struct.point.Point3D_F64;
+import georegression.struct.se.Se3_F64;
+import georegression.struct.se.SpecialEuclideanOps_F64;
+import georegression.transform.se.SePointOps_F64;
+import org.ddogleg.struct.DogArray_I32;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import java.util.ArrayList;
+import java.util.List;
 
-public class TestEpipolarScoreKnownExtrinsics {
-	@Test void implement() {
-		fail("Implement");
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TestEpipolarScoreKnownExtrinsics extends BoofStandardJUnit {
+	private final CameraPinholeBrown intrinsic1 = new CameraPinholeBrown(400, 400, 0, 500, 500, 1000, 1000).fsetRadial(0.0, 0.0);
+	private final CameraPinholeBrown intrinsic2 = new CameraPinholeBrown(600, 600, 0, 600, 600, 1000, 1000).fsetRadial(0.0, 0.0);
+	private final int numPoints = 100;
+	private final List<Point3D_F64> feats3D = UtilPoint3D_F64.random(new Point3D_F64(0, 0, 1), -0.5, 0.5, numPoints, rand);
+
+	/**
+	 * Compute the score for different scenarios and see if there is a higher score when there is more motion
+	 */
+	@Test void largerMotionHigherScore() {
+		// run scenarios with different amounts of translation between the views
+		double score1 = scoreScenario(SpecialEuclideanOps_F64.eulerXyz(0.5, 0, 0, 0, 0, 0, null), true);
+		double score2 = scoreScenario(SpecialEuclideanOps_F64.eulerXyz(0.1, 0, 0, 0, 0, 0, null), true);
+		double score3 = scoreScenario(SpecialEuclideanOps_F64.eulerXyz(0.0, 0, 0, 0, 0, 0, null), false);
+
+		assertTrue(score1 > score2);
+		assertTrue(score2 > score3);
+	}
+
+	private double scoreScenario( Se3_F64 a_to_b, boolean is3D ) {
+		List<Point3D_F64> observations1 = new ArrayList<>();
+		List<Point3D_F64> observations2 = new ArrayList<>();
+		List<AssociatedIndex> pairs = new ArrayList<>();
+
+		for (int i = 0; i < feats3D.size(); i++) {
+			Point3D_F64 X1 = feats3D.get(i).copy();
+			Point3D_F64 X2 = new Point3D_F64();
+
+			SePointOps_F64.transform(a_to_b, X1, X2);
+
+			// Add a tiny bit of noise
+			X1.z += rand.nextGaussian()*0.002;
+			X2.z += rand.nextGaussian()*0.002;
+
+			// Create pointing vector observations from the location of the feature in each reference frame
+			X1.divideIP(X1.norm());
+			X2.divideIP(X2.norm());
+
+			observations1.add(X1);
+			observations2.add(X2);
+
+			pairs.add(new AssociatedIndex(i, i));
+		}
+
+		Point3Transform2_F64 lens1 = new SphereToNarrowPixel_F64(LensDistortionFactory.
+				narrow(intrinsic1).distort_F64(false, true));
+		Point3Transform2_F64 lens2 = new SphereToNarrowPixel_F64(LensDistortionFactory.
+				narrow(intrinsic1).distort_F64(false, true));
+
+		var inliersIdx = new DogArray_I32();
+		var alg = new EpipolarScoreKnownExtrinsics();
+//		alg.maxReprojectionError.setRelative(0.0001, 1);
+		alg.process(intrinsic1.getDimension(null), intrinsic2.getDimension(null),
+				lens1, lens2, observations1, observations2, pairs, a_to_b, inliersIdx);
+
+		if (is3D)
+			assertEquals(numPoints, inliersIdx.size);
+		assertEquals(is3D, alg.is3D);
+		return alg.getScore();
 	}
 }
