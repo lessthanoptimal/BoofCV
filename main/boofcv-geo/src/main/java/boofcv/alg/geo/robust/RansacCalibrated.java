d27c955dce1c25b852980665bcee23cf7f027415
Peter Abeles <peter.abeles@gmail.com>
2022-06-27 11:05:16 -0700

    Modified RansacCalibrated so that it supports concurrency

--- main/boofcv-geo/src/main/java/boofcv/alg/geo/robust/RansacCalibrated.java
+++ main/boofcv-geo/src/main/java/boofcv/alg/geo/robust/RansacCalibrated.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021, Peter Abeles. All Rights Reserved.
+ * Copyright (c) 2022, Peter Abeles. All Rights Reserved.
  *
  * This file is part of BoofCV (http://boofcv.org).
  *
@@ -20,35 +20,84 @@ package boofcv.alg.geo.robust;
 
 import boofcv.alg.geo.DistanceFromModelMultiView;
 import boofcv.struct.calib.CameraPinhole;
-import org.ddogleg.fitting.modelset.ModelGenerator;
-import org.ddogleg.fitting.modelset.ModelManager;
+import lombok.Getter;
 import org.ddogleg.fitting.modelset.ransac.Ransac;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Extension of {@link Ransac} for two calibrated camera views. Input point will be in normalized image coordinates
  *
  * @author Peter Abeles
  */
-public class RansacCalibrated<Model, Point> extends Ransac<Model, Point>
-		implements ModelMatcherMultiview<Model, Point> {
-	private DistanceFromModelMultiView<Model, Point> modelDistance;
+public class RansacCalibrated<Model, Point> implements ModelMatcherMultiview<Model, Point> {
 
-	public RansacCalibrated( long randSeed, int maxIterations, double thresholdFit,
-							 ModelManager<Model> modelManager,
-							 ModelGenerator<Model, Point> modelGenerator,
-							 DistanceFromModelMultiView<Model, Point> modelDistance ) {
-		super(randSeed, maxIterations, thresholdFit, modelManager, modelDistance.getPointType());
-		this.modelDistance = modelDistance;
-		setModel(() -> modelGenerator, () -> modelDistance);
+	/** Underlying RANSAC implementation */
+	@Getter Ransac<Model, Point> ransac;
+
+	// All the passed in camera intrinsics
+	final List<CameraPinhole> listIntrinsics = new ArrayList<>();
+
+	public RansacCalibrated( Ransac<Model, Point> ransac, int numberOfViews ) {
+		this.ransac = ransac;
+
+		// Initialize the list
+		for (int i = 0; i < numberOfViews; i++) {
+			listIntrinsics.add(null);
+		}
+
+		ransac.setInitializeModels(( generator, distance ) -> {
+			DistanceFromModelMultiView dist = (DistanceFromModelMultiView)distance;
+			for (int viewIdx = 0; viewIdx < listIntrinsics.size(); viewIdx++) {
+				dist.setIntrinsic(viewIdx, listIntrinsics.get(viewIdx));
+			}
+		});
 	}
 
 	@Override
 	public void setIntrinsic( int view, CameraPinhole intrinsic ) {
-		this.modelDistance.setIntrinsic(view, intrinsic);
+		listIntrinsics.set(view, intrinsic);
 	}
 
 	@Override
 	public int getNumberOfViews() {
-		return modelDistance.getNumberOfViews();
+		return listIntrinsics.size();
+	}
+
+	@Override public boolean process( List<Point> dataSet ) {
+		return ransac.process(dataSet);
+	}
+
+	@Override public Model getModelParameters() {
+		return ransac.getModelParameters();
+	}
+
+	@Override public List<Point> getMatchSet() {
+		return ransac.getMatchSet();
+	}
+
+	@Override public int getInputIndex( int matchIndex ) {
+		return ransac.getInputIndex(matchIndex);
+	}
+
+	@Override public double getFitQuality() {
+		return ransac.getFitQuality();
+	}
+
+	@Override public int getMinimumSize() {
+		return ransac.getMinimumSize();
+	}
+
+	@Override public void reset() {
+		ransac.reset();
+	}
+
+	@Override public Class<Point> getPointType() {
+		return ransac.getPointType();
+	}
+
+	@Override public Class<Model> getModelType() {
+		return ransac.getModelType();
 	}
 }
