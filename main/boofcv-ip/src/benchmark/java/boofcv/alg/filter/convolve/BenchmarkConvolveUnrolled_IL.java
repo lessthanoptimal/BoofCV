ac10b56eddb47aea47f1973726db5bb2561aabeb
Peter1169 <46818665+Peter1169@users.noreply.github.com>
2026-05-21 07:55:58 +0200

    Add unrolled interleaved convolution kernels

---
+++ main/boofcv-ip/src/benchmark/java/boofcv/alg/filter/convolve/BenchmarkConvolveUnrolled_IL.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright (c) 2026, Peter Abeles. All Rights Reserved.
+ *
+ * This file is part of BoofCV (http://boofcv.org).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package boofcv.alg.filter.convolve;
+
+import boofcv.alg.filter.convolve.noborder.ConvolveImageUnrolled_IL;
+import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+import org.openjdk.jmh.runner.options.TimeValue;
+
+import java.util.concurrent.TimeUnit;
+
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.MILLISECONDS)
+@Warmup(iterations = 2)
+@Measurement(iterations = 5)
+@State(Scope.Benchmark)
+@Fork(value = 1)
+@SuppressWarnings({"UnusedDeclaration"})
+public class BenchmarkConvolveUnrolled_IL extends CommonBenchmarkConvolve_IL {
+	@Param({"1", "2", "3"})
+	private int radius;
+
+	@Param({"2", "3", "4"})
+	private int bands;
+
+	@Setup public void setup() {
+		reshape(bands);
+		super.setup(radius);
+	}
+
+	private void reshape( int bands ) {
+		numBands = bands;
+		input_F32.reshape(width, height, bands);
+		input_F64.reshape(width, height, bands);
+		input_U8.reshape(width, height, bands);
+		input_U16.reshape(width, height, bands);
+		input_S16.reshape(width, height, bands);
+		input_S32.reshape(width, height, bands);
+		out_F32.reshape(width, height, bands);
+		out_F64.reshape(width, height, bands);
+		out_U8.reshape(width, height, bands);
+		out_S16.reshape(width, height, bands);
+		out_S32.reshape(width, height, bands);
+	}
+
+	// @formatter:off
+	@Benchmark public boolean horizontal_F32() {return ConvolveImageUnrolled_IL.horizontal(kernelF32, input_F32, out_F32);}
+	@Benchmark public boolean vertical_F32() {return ConvolveImageUnrolled_IL.vertical(kernelF32, input_F32, out_F32);}
+	@Benchmark public boolean convolve2D_F32() {return ConvolveImageUnrolled_IL.convolve(kernel2D_F32, input_F32, out_F32);}
+	@Benchmark public boolean horizontal_U8_I16() {return ConvolveImageUnrolled_IL.horizontal(kernelI32, input_U8, out_S16);}
+	@Benchmark public boolean vertical_U8_I16() {return ConvolveImageUnrolled_IL.vertical(kernelI32, input_U8, out_S16);}
+	@Benchmark public boolean convolve2D_U8_I16() {return ConvolveImageUnrolled_IL.convolve(kernel2D_I32, input_U8, out_S16);}
+	// @formatter:on
+
+	public static void main( String[] args ) throws RunnerException {
+		Options opt = new OptionsBuilder()
+				.include(BenchmarkConvolveUnrolled_IL.class.getSimpleName())
+				.warmupTime(TimeValue.seconds(1))
+				.measurementTime(TimeValue.seconds(1))
+				.build();
+
+		new Runner(opt).run();
+	}
+}
